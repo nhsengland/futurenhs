5933e2bac0229818d687291e69f35449cede6cdf
Tim Hunt <tim.hunt@cds.co.uk>
2021-06-23 15:36:47 +0100

    Bug fix for tab menu as it was recursively calling itself

--- MVCForum.Website/ViewModels/Shared/NavItemBase.cs
+++ MVCForum.Website/ViewModels/Shared/NavItemBase.cs
@@ -5,11 +5,13 @@ using System.Web;
 
 namespace MvcForum.Web.ViewModels.Shared
 {
+
     /// <summary>
     /// Defines the base class for a navigation item e.g. a link or tab.
     /// </summary>
     public abstract class NavItemBase
     {
+        private bool ActiveTab;
         /// <summary>
         /// Gets or sets the NavItem name.
         /// </summary>
@@ -44,13 +46,17 @@ namespace MvcForum.Web.ViewModels.Shared
         /// Gets or sets boolean flag for active.
         /// </summary>
         public bool Active {
-            get {
-                return !string.IsNullOrEmpty(Url) ? HttpContext.Current.Request.Url.LocalPath.Equals(Url) : false;
-            } 
-            set
+            get
             {
-                Active = value;
+                if (!ActiveTab)
+                {
+                    return !string.IsNullOrEmpty(Url) && HttpContext.Current.Request.Url.LocalPath.Equals(Url);
+                }
+
+                return ActiveTab;
             }
+
+            set => ActiveTab = value;
         }
     }
 }
