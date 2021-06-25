85d46f6ddabfd729d54ccce199464c95dc93a496
Tim Hunt <tim.hunt@cds.co.uk>
2021-06-25 15:02:35 +0100

    [+] Addition of app insights SDK

--- MVCForum.Website/Global.asax.cs
+++ MVCForum.Website/Global.asax.cs
@@ -11,32 +11,33 @@ namespace MvcForum.Web
     public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
-        {   
-            var instrumentationKey = WebConfigurationManager.AppSettings["APPINSIGHTS_INSTRUMENTATIONKEY"];
-            // Create a TelemetryConfiguration instance.
-            TelemetryConfiguration config = TelemetryConfiguration.CreateDefault();
-            config.InstrumentationKey = instrumentationKey;
-            QuickPulseTelemetryProcessor quickPulseProcessor = null;
-            config.DefaultTelemetrySink.TelemetryProcessorChainBuilder
-                .Use((next) =>
-                {
-                    quickPulseProcessor = new QuickPulseTelemetryProcessor(next);
-                    return quickPulseProcessor;
-                })
-                .Build();
+        {
+            Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration.Active.InstrumentationKey = System.Web.Configuration.WebConfigurationManager.AppSettings["APPINSIGHTS_INSTRUMENTATIONKEY"];
+            //var instrumentationKey = WebConfigurationManager.AppSettings["APPINSIGHTS_INSTRUMENTATIONKEY"];
+            //// Create a TelemetryConfiguration instance.
+            //TelemetryConfiguration config = TelemetryConfiguration.CreateDefault();
+            //config.InstrumentationKey = instrumentationKey;
+            //QuickPulseTelemetryProcessor quickPulseProcessor = null;
+            //config.DefaultTelemetrySink.TelemetryProcessorChainBuilder
+            //    .Use((next) =>
+            //    {
+            //        quickPulseProcessor = new QuickPulseTelemetryProcessor(next);
+            //        return quickPulseProcessor;
+            //    })
+            //    .Build();
 
-            var quickPulseModule = new QuickPulseTelemetryModule();
+            //var quickPulseModule = new QuickPulseTelemetryModule();
 
-            // Secure the control channel.
-            // This is optional, but recommended.
-            quickPulseModule.AuthenticationApiKey = "APPINSIGHTS_APIKEY";
-            quickPulseModule.Initialize(config);
-            quickPulseModule.RegisterTelemetryProcessor(quickPulseProcessor);
+            //// Secure the control channel.
+            //// This is optional, but recommended.
+            //quickPulseModule.AuthenticationApiKey = "APPINSIGHTS_APIKEY";
+            //quickPulseModule.Initialize(config);
+            //quickPulseModule.RegisterTelemetryProcessor(quickPulseProcessor);
 
-            // Create a TelemetryClient instance. It is important
-            // to use the same TelemetryConfiguration here as the one
-            // used to setup Live Metrics.
-            TelemetryClient client = new TelemetryClient(config);
+            //// Create a TelemetryClient instance. It is important
+            //// to use the same TelemetryConfiguration here as the one
+            //// used to setup Live Metrics.
+            //TelemetryClient client = new TelemetryClient(config);
         }
     }
 }
