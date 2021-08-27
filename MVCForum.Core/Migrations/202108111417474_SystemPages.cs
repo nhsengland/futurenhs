e5452492201855b3dafee9cccd54b182dad441df
Mathew Beldon <mathew.beldon@cds.co.uk>
2021-08-27 10:04:03 +0100

    Adds explicit maxLength to SystemPages column

--- MVCForum.Core/Migrations/202108111417474_SystemPages.cs
+++ MVCForum.Core/Migrations/202108111417474_SystemPages.cs
@@ -13,9 +13,9 @@ namespace MvcForum.Core.Services.Migrations
                 {
                     Id = c.Guid(nullable: false),
                     //Set the max length for the column to 50 characters
-                    Slug = c.String(nullable: false, 50),
+                    Slug = c.String(nullable: false, maxLength: 50),
                     //Set the max length for the column to 100 characters
-                    Title = c.String(nullable: false, 100),
+                    Title = c.String(nullable: false, maxLength: 100),
                     Content = c.String(nullable: false),
                     IsDeleted = c.Boolean(nullable: false),
                 })
