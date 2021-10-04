22e529ca23b406041ec2a515e172f8a9ec81b099
Mathew Beldon <mathew.beldon@cds.co.uk>
2021-10-04 16:41:12 +0100

    Story 72235 Tests
    [+] Adds tests for GroupAddMember
    [+] Adds tests for GroupInviteMember

---
+++ MvcForum.Web.Tests/Controllers/Base/SetUserInContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MvcForum.Web.Tests.Controllers.Base
+{
+    public class SetUserInContext
+    {
+        public static void SetContext(string username)
+        {
+            HttpContext.Current = new HttpContext(
+            new HttpRequest("", "http://tempuri.org ", ""),
+            new HttpResponse(new StringWriter())
+            );
+
+            // User is logged in
+            HttpContext.Current.User = new GenericPrincipal(
+                new GenericIdentity(username),
+                new string[0]
+                );
+        }
+    }
+}
