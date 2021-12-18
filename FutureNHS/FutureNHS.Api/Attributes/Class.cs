b6fad7b640ced21b4817a1ef2280bb6012fbbc0b
Tim Hunt <tim.hunt@cds.co.uk>
2021-12-18 23:48:18 +0000

    api key auth

---
+++ FutureNHS/FutureNHS.Api/Attributes/Class.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FutureNHS.Api.Attributes
+{
+    [AttributeUsage(validOn: AttributeTargets.Class)]
+    public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string Apikeyname = "ApiKey";
+        public async Task OnActionExecutionAsync
+            (ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue
+                (Apikeyname, out var extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Api Key was not provided"
+                };
+                return;
+            }
+
+            var appSettings =
+                context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+
+            var apiKey = appSettings.GetValue<string>(Apikeyname);
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Api Key is not valid"
+                };
+                return;
+            }
+
+            await next();
+        }
+    }
+}
