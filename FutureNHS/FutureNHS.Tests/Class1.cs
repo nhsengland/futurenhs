2b4d4fc5a9d88057cbea5a8f32c63d666de901ed
Tim Hunt <tim.hunt@cds.co.uk>
2021-12-27 15:28:09 +0000

    Add AzureSqlDbConnectionFactory unit tests

---
+++ FutureNHS/FutureNHS.Tests/Class1.cs
@@ -0,0 +1,109 @@
+﻿using FutureNHS.Api.DataAccess.Repositories.Database.DatabaseProviders;
+using FutureNHS.Api.DataAccess.Repositories.Database.DatabaseProviders.Interfaces;
+using FutureNHS.Infrastructure.Repositories.Database.RetryPolicy;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FutureNHS_WOPI_Host_UnitTests.PlatformHelpers
+{
+    [TestClass]
+    public sealed class AzureSqlDbConnectionFactoryTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public async Task GetReadOnlyConnectionAsync_ThrowsIfInvalidStructureForConnectionString()
+        {
+            var cancellationToken = CancellationToken.None;
+
+            var logger = new Moq.Mock<ILogger<AzureSqlDbConnectionFactory>>().Object;
+            var retryPolicy = new Moq.Mock<DbRetryPolicy>().Object;
+
+            IAzureSqlDbConnectionFactory azureSqlDbConnectionFactory = new AzureSqlDbConnectionFactory("read-write", "read-only", retryPolicy, logger);
+
+            _ = await azureSqlDbConnectionFactory.GetReadOnlyConnectionAsync(cancellationToken);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OperationCanceledException))]
+        public async Task GetReadOnlyConnectionAsync_ThrowsIfCancelled()
+        {
+            var cts = new CancellationTokenSource();
+
+            var cancellationToken = cts.Token;
+
+            var logger = new Moq.Mock<ILogger<AzureSqlDbConnectionFactory>>().Object;
+            var retryPolicy = new Moq.Mock<DbRetryPolicy>().Object;
+
+            cts.Cancel();
+
+            IAzureSqlDbConnectionFactory azureSqlDbConnectionFactory = new AzureSqlDbConnectionFactory("read-write", "read-only", retryPolicy, logger);
+
+            _ = await azureSqlDbConnectionFactory.GetReadOnlyConnectionAsync(cancellationToken);
+        }
+
+        [TestMethod]
+        public async Task GetReadOnlyConnectionAsync_DoesNotThrowIfInvalidConnectionString_ThrowsOnOpen()
+        {
+            var cancellationToken = CancellationToken.None;
+
+            var logger = new Moq.Mock<ILogger<AzureSqlDbConnectionFactory>>().Object;
+            var retryPolicy = new Moq.Mock<DbRetryPolicy>().Object;
+
+            var INVALID_READONLY_CONNECTIONSTRING = "Server=tcp:" + Guid.NewGuid().ToString() + ".database.windows.net,1433;Initial Catalog=initial-catalog;Persist Security Info=False;User ID=my-user-id;Password=my-password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;ApplicationIntent=ReadOnly";
+
+            IAzureSqlDbConnectionFactory azureSqlDbConnectionFactory = new AzureSqlDbConnectionFactory("read-write", INVALID_READONLY_CONNECTIONSTRING, retryPolicy, logger);
+
+            _ = await azureSqlDbConnectionFactory.GetReadOnlyConnectionAsync(cancellationToken);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public async Task GetReadWriteConnectionAsync_ThrowsIfInvalidStructureForConnectionString()
+        {
+            var cancellationToken = CancellationToken.None;
+
+            var logger = new Moq.Mock<ILogger<AzureSqlDbConnectionFactory>>().Object;
+            var retryPolicy = new Moq.Mock<DbRetryPolicy>().Object;
+
+            IAzureSqlDbConnectionFactory azureSqlDbConnectionFactory = new AzureSqlDbConnectionFactory("read-write", "read-only", retryPolicy, logger);
+
+            _ = await azureSqlDbConnectionFactory.GetReadWriteConnectionAsync(cancellationToken);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OperationCanceledException))]
+        public async Task GetReadWriteConnectionAsync_ThrowsIfCancelled()
+        {
+            var cts = new CancellationTokenSource();
+
+            var cancellationToken = cts.Token;
+
+            var logger = new Moq.Mock<ILogger<AzureSqlDbConnectionFactory>>().Object;
+            var retryPolicy = new Moq.Mock<DbRetryPolicy>().Object;
+
+            cts.Cancel();
+
+            IAzureSqlDbConnectionFactory azureSqlDbConnectionFactory = new AzureSqlDbConnectionFactory("read-write", "read-only", retryPolicy, logger);
+
+            _ = await azureSqlDbConnectionFactory.GetReadWriteConnectionAsync(cancellationToken);
+        }
+
+        [TestMethod]
+        public async Task GetReadWriteConnectionAsync_DoesNotThrowIfInvalidConnectionString_ThrowsOnOpen()
+        {
+            var cancellationToken = CancellationToken.None;
+
+            var logger = new Moq.Mock<ILogger<AzureSqlDbConnectionFactory>>().Object;
+            var retryPolicy = new Moq.Mock<DbRetryPolicy>().Object;
+
+            var INVALID_READWRITE_CONNECTIONSTRING = "Server=tcp:" + Guid.NewGuid().ToString() + ".database.windows.net,1433;Initial Catalog=initial-catalog;Persist Security Info=False;User ID=my-user-id;Password=my-password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            IAzureSqlDbConnectionFactory azureSqlDbConnectionFactory = new AzureSqlDbConnectionFactory(INVALID_READWRITE_CONNECTIONSTRING, "read-only", retryPolicy, logger);
+
+            _ = await azureSqlDbConnectionFactory.GetReadWriteConnectionAsync(cancellationToken);
+        }
+    }
+}
