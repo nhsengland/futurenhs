a7e6b1a57684a69d5ff70805285aa42ee6b5a1ca
Timi Abel <timiabel.dev@gmail.com>
2022-01-28 22:16:40 +0000

    added in safari modal

--- MVCForum/MVCForum.Website/UI/assets/src/ts/root/polyfills.ts
+++ MVCForum/MVCForum.Website/UI/assets/src/ts/root/polyfills.ts
@@ -1,35 +1,9 @@
-import dialogPolyfill from "dialog-polyfill";
+import dialogPolyfill from '@modules/polyfills/dialog';
 
+const dialogElements: Array<Element> = Array.from(document.getElementsByTagName('dialog'));
 
-const dialogElement: any = document.querySelector('dialog');
-
+dialogElements.forEach( dialogElement => {
 
     dialogPolyfill.registerDialog(dialogElement);
 
-
-if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-  const destination = document.getElementsByTagName('body')[0];
-  // Declare a fragment:
-  const fragment = document.createDocumentFragment();
-  
-  fragment.appendChild(dialogElement);
-
-  destination.insertBefore(fragment, destination.firstChild);
-}
-
-if (typeof(dialogElement) != 'undefined' && dialogElement != null && typeof dialogElement.showModal !== "function") {
-   
-    const polyfill = document.createElement("script");
-    polyfill.type = "text/javascript";
-    polyfill.src = "@modules/dialog-polyfill/dist/dialog-polyfill.js";
-    document.body.append(polyfill);
-  
-    const polyfillStyles = document.createElement("link");
-    polyfillStyles.rel = "stylesheet";
-    polyfillStyles.href = "@modules/dialog-polyfill/dialog-polyfill.css";
-    document.head.append(polyfillStyles); 
-
-  // Register polyfill on dialog element once the script has loaded
-  dialogPolyfill.registerDialog(dialogElement);
-}
-
+});
