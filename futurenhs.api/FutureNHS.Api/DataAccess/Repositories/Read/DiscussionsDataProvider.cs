3fbd7803c3f9386037d84440c9adeec1a6ee35a3
Tim Hunt <tim.hunt@cds.co.uk>
2022-02-23 11:06:32 +0000

    [Api] sort by is sticky then date

--- futurenhs.api/FutureNHS.Api/DataAccess/Repositories/Read/DiscussionsDataProvider.cs
+++ futurenhs.api/FutureNHS.Api/DataAccess/Repositories/Read/DiscussionsDataProvider.cs
@@ -67,7 +67,7 @@ namespace FutureNHS.Api.DataAccess.Repositories.Read
 
                     WHERE       groups.Slug = @Slug
                     AND         groups.IsDeleted = 0       
-                    ORDER BY    discussion.IsSticky ASC, discussion.CreatedAtUTC DESC
+                    ORDER BY    discussion.IsSticky DESC, discussion.CreatedAtUTC DESC
 
                     OFFSET      @Offset ROWS
                     FETCH NEXT  @Limit ROWS ONLY;
