8b87ec3d9aca83b8e975242f5dd83f0c85385c74
Richard Iles <richard.iles@digital.cds.uk>
2022-03-29 08:11:59 +0100

    [+] Adds text block to add block screen in page manager

--- futurenhs.app/components/PageManager/index.tsx
+++ futurenhs.app/components/PageManager/index.tsx
@@ -24,7 +24,10 @@ export const PageManager: (props: Props) => JSX.Element = ({
 
     const generatedClasses: any = {
         wrapper: classNames(className),
-        block: classNames('c-page-manager-block', 'u-text-center')
+        addBlock: classNames('c-page-manager-block', 'u-text-center'),
+        block: classNames('c-page-manager-block'),
+        blockHeader: classNames('c-page-manager-block_header', 'u-text-bold'),
+        blockBody: classNames('c-page-manager-block_body')
     };
 
     const handleAddBlock = (event: any): void => {
@@ -44,15 +47,30 @@ export const PageManager: (props: Props) => JSX.Element = ({
     return (
 
         <div className={generatedClasses.wrapper}>
+            {isInAddBlockMode &&
+                <ul className="u-list-none u-p-0">
+                    <li>
+                        <div className={generatedClasses.block}>
+                            <header className={generatedClasses.blockHeader}>Text block</header>
+                            <div className={generatedClasses.blockBody}>
+                                <h3 className="nhsuk-heading-l">Example text block title here</h3>
+                                <p>Here’s an example of a text block and how it will look on the platform. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            }
             {isInAddBlockMode
 
                 ?   <button onClick={handleAddBlockCancel} className="c-button c-button-outline u-drop-shadow">Cancel</button>
 
-                :   <div className={generatedClasses.block}>
-                        <button onClick={handleAddBlock} className="c-button c-button-outline u-drop-shadow">
-                            <SVGIcon name="icon-add-content" className="u-w-9 u-h-8 u-mr-4 u-align-middle" />
-                            <span className="u-align-middle">Add content block</span>
-                        </button>
+                :   <div className={generatedClasses.addBlock}>
+                        <div className={generatedClasses.blockBody}>
+                            <button onClick={handleAddBlock} className="c-button c-button-outline u-drop-shadow">
+                                <SVGIcon name="icon-add-content" className="u-w-9 u-h-8 u-mr-4 u-align-middle" />
+                                <span className="u-align-middle">Add content block</span>
+                            </button>
+                        </div>
                     </div>
 
             }
