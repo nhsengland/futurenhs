65fc47dac8b1c7cb9994c920ab890da6cc3ae9b4
Jack Williams <jack.williams@cds.co.uk>
2022-04-05 12:22:39 +0100

    Checks whether requested content requires deserialization before returning data to main web api

--- futurenhs.content.api/Umbraco9ContentApi.Core/Converters/JsonContentConverter.cs
+++ futurenhs.content.api/Umbraco9ContentApi.Core/Converters/JsonContentConverter.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Umbraco9ContentApi.Core.Models;
 
 namespace UmbracoContentApi.Core.Converters
@@ -9,7 +10,38 @@ namespace UmbracoContentApi.Core.Converters
 
         public object Convert(object value, Dictionary<string, object>? options = null)
         {
-            return JsonConvert.DeserializeObject<PageContentModelData>(value as string);
+            return IsValidJson(value as string)
+                ? JsonConvert.DeserializeObject<PageContentModelData>(value as string)
+                : value;
+        }
+
+        /// <summary>
+        /// Determines whether [is valid json] [the specified string input].
+        /// </summary>
+        /// <param name="strInput">The string input.</param>
+        /// <returns>
+        ///   <c>true</c> if [is valid json] [the specified string input]; otherwise, <c>false</c>.
+        /// </returns>
+        private bool IsValidJson(string strInput)
+        {
+            if (string.IsNullOrWhiteSpace(strInput))
+            {
+                return false;
+            }
+
+            strInput = strInput.Trim();
+            if ((strInput.StartsWith("{") && strInput.EndsWith("}")) || //For object
+                (strInput.StartsWith("[") && strInput.EndsWith("]"))) //For array
+            {
+                var obj = JToken.Parse(strInput);
+
+                if (obj is not null)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
