ca343db8fa6f25aaa5a3df64a3a5ce1b7fa824a5
Alex Grubba <Alex.Grubba@cds.co.uk>
2022-04-05 16:48:29 +0100

    [*] updated slack webhook to continue output of test results [*] updated customCommands for new privateBeta URL

--- futurenhs.automation/customCommands.js
+++ futurenhs.automation/customCommands.js
@@ -1,15 +1,15 @@
 {
     var exec = require('child-process-promise').exec;
     let obj = {
-        "core": `wdio wdio.headless.conf.js --suite=smokeTest --cucumberOpts.tagExpression="@Core and not @Pending"`,
-        "pipeline": "wdio wdio.headless.conf.js --suite=fullRegression",
-        "local": "wdio wdio.headless.conf.js --baseUrl=http://localhost:5000/ --suite=fullRegression",
-        "devTest": `wdio wdio.headless.conf.js --baseUrl=https://futurenhs.cds.co.uk/ --suite=fullRegression`,
+        "core": `wdio wdio.headless.conf.js --suite=smokeTest --cucumberOpts.tagExpression="@Core and not @Pending and not @NotLocal"`,
+        "pipeline": `wdio wdio.headless.conf.js --suite=fullRegression`,
+        "local": `wdio wdio.headless.conf.js --baseUrl=http://localhost:5000/ --suite=fullRegression --cucumberOpts.tagExpression="not @Pending and not @NotInLocal"`,
+        "devTest": `wdio wdio.headless.conf.js --baseUrl=https://collaborate-dev.future.nhs.uk/ --suite=fullRegression`,
         "uat": `wdio wdio.headless.conf.js --baseUrl=https://collaborate-uat.future.nhs.uk/ --suite=fullRegression`,
-        "firefox": "wdio wdio.firefox.conf.js --suite=fullRegression",
-        "msedge": "wdio wdio.msedge.conf.js --suite=fullRegression",
-        "visualRegression": "wdio --spec=./features/visualRegression.feature",
-        "AllureReport": "allure generate allureResults --clean -o allureReport",
+        "firefox": `wdio wdio.firefox.conf.js --suite=fullRegression`,
+        "msedge": `wdio wdio.msedge.conf.js --suite=fullRegression`,
+        "visualRegression": `wdio --spec=./features/visualRegression.feature`,
+        "AllureReport": `allure generate allureResults --clean -o allureReport`,
     }
     var promise = exec(obj[process.argv[2]]);
 
