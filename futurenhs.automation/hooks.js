4b2febe2dd741e4b52906e8bb3e446e928252a8f
Alex Grubba <Alex.Grubba@cds.co.uk>
2022-04-11 13:38:52 +0100

    [-] removing deprecated code

--- futurenhs.automation/hooks.js
+++
@@ -1,7 +0,0 @@
-var {After, Before} = require('@cucumber/cucumber');
-const adminPage = require('./pageObjects/adminPage');
-
-Before({tags: "@createSystemPages"}, function() {
-    console.log('***************************************');
-    expect(adminPage.createSystemPages()).toEqual(true)
-});
