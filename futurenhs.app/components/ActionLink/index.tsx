148d68cdcc5050ac3e1fb83ae589f9c1a6776520
ReallySmall <richard.iles@digital.cds.uk>
2022-04-12 17:48:14 +0100

    [+] Adds ActionLink component to enforce consistent use of aria labels and styles in action links [*] Replaces instances of manually created action links in tables with ActionLink component

---
+++ futurenhs.app/components/ActionLink/index.tsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames'
+
+import { Link } from '@components/Link'
+import { SVGIcon } from '@components/SVGIcon'
+
+import { Props } from './interfaces'
+
+export const ActionLink: (props: Props) => JSX.Element = ({
+    href,
+    text,
+    iconName,
+    className,
+}) => {
+    const { body, ariaLabel } = text ?? {}
+
+    const generatedClasses: any = {
+        wrapper: classNames('u-align-middle', className),
+        icon: classNames('u-align-middle', 'u-w-4 u-h-4 u-mr-1 u-fill-theme-0'),
+    }
+
+    return (
+        <Link href={href}>
+            <a className={generatedClasses.wrapper} aria-label={ariaLabel}>
+                {iconName &&
+                    <SVGIcon
+                        name={iconName}
+                        className={generatedClasses.icon}
+                    />
+                }
+                {body}
+            </a>
+        </Link>
+    )
+}
