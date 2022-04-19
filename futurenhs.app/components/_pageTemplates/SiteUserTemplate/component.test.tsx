1f4a1707788eae932c2431fe3d4b4fd46076bc99
Andy Smith <andrew.smith@cds.co.uk>
2022-04-19 13:34:26 +0100

    [+] Adds unit tests for admin portal page templates

--- futurenhs.app/components/_pageTemplates/SiteUserTemplate/component.test.tsx
+++ futurenhs.app/components/_pageTemplates/SiteUserTemplate/component.test.tsx
@@ -1,13 +1,16 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, cleanup } from '@testing-library/react'
 import { SiteUserTemplate } from './index'
 import { Props } from './interfaces'
 import * as nextRouter from 'next/router'
 import forms from '@formConfigs/index'
 import { routes } from '@jestMocks/generic-props'
+import { actions } from '@constants/actions'
 
 
 describe('Site User Template', () => {
-
+    
+    afterEach(cleanup)
+    
     ;(nextRouter as any).useRouter = jest.fn()
     ;(nextRouter as any).useRouter.mockImplementation(() => ({
         asPath: '/users/userId',
@@ -27,7 +30,14 @@ describe('Site User Template', () => {
             firstName: 'MockFirstName',
             lastName: 'MockLastName',
         },
-        contentText: {} as any,
+        contentText: {
+            editHeading: 'Edit profile',
+            editButtonLabel: 'Edit button',
+            firstNameLabel: 'First name',
+            lastNameLabel: 'Last name',
+            pronounsLabel: 'he/him',
+            emailLabel: 'Email'
+        },
         actions: [],
         forms: forms,
         routes: routes
@@ -38,5 +48,47 @@ describe('Site User Template', () => {
 
         expect(screen.getAllByText(props.siteUser.firstName).length).toBe(1)
         expect(screen.getAllByText(props.siteUser.lastName).length).toBe(1)
+    })
+
+    it('conditionally renders edit profile button', () => {
+
+        render(<SiteUserTemplate { ...props }/>);
+
+        expect(screen.queryByText('Edit button')).toBeNull();
+
+        const propsCopy: Props = Object.assign({}, props, {
+            actions: [
+                actions.SITE_ADMIN_MEMBERS_EDIT
+            ]
+        })
+
+        render(<SiteUserTemplate { ...propsCopy }/>);
+
+        expect(screen.getAllByText('Edit button').length).toBe(1);
+
+    })
+
+    it('conditionally renders edit user form', () => {
+
+        render(<SiteUserTemplate { ...props }/>);
+
+        expect(screen.queryByText('Edit profile')).toBeNull();
+
+        ;(nextRouter as any).useRouter.mockImplementation(() => ({
+            asPath: '/users/userId',
+            query: {
+                edit: true
+            }
+        }))
+
+        const propsCopy: Props = Object.assign({}, props, {
+            actions: [
+                actions.SITE_ADMIN_MEMBERS_EDIT
+            ]
+        })
+
+        render(<SiteUserTemplate { ...propsCopy }/>);
+
+        expect(screen.getAllByText('Edit profile').length).toBe(1)
     })
 })
