04f1c2180d347825652afb39d73b9d7d0663550e
Jack Williams <jack.williams@cds.co.uk>
2022-04-26 09:06:23 +0100

    [+] contentapi backend validation for page updates

---
+++ futurenhs.content.api/Umbraco9ContentApi.Core/Services/FutureNhs/FutureNhsValidationService.cs
@@ -0,0 +1,39 @@
+﻿namespace Umbraco9ContentApi.Core.Services.FutureNhs
+{
+    using System;
+    using Umbraco.Cms.Core.Services;
+    using Umbraco9ContentApi.Core.Models;
+    using Umbraco9ContentApi.Core.Services.FutureNhs.Interface;
+
+    public class FutureNhsValidationService : IFutureNhsValidationService
+    {
+        private readonly IContentTypeService _contentTypeService;
+
+        public FutureNhsValidationService(IContentTypeService contentTypeService)
+        {
+            _contentTypeService = contentTypeService ?? throw new ArgumentNullException(nameof(contentTypeService));
+        }
+
+        public void ValidatePageContentModel(PageContentModel pageContentModel)
+        {
+            foreach (var block in pageContentModel.Blocks)
+            {
+                var contentType = _contentTypeService.Get(block.Item?.ContentType);
+
+                if (contentType is null)
+                {
+                    throw new ArgumentOutOfRangeException($"{block.Item?.ContentType} isn't a valid content type.");
+                }
+
+                var expectedValues = contentType.PropertyTypes.Select(x => x.Alias).ToList();
+                var blockValues = block.Content?.Select(x => x.Key.ToString()).ToList();
+
+                if (blockValues is not null)
+                {
+                    if (!expectedValues.SequenceEqual(blockValues))
+                        throw new ArgumentOutOfRangeException($"Block fields do not match the expected fields for contentType {contentType.Name}.");
+                }
+            }
+        }
+    }
+}
