731b4137187fd06ecf65ab6dd01317f8c5a3f1a8
Tim-Hunt-CDS <81264354+Tim-Hunt-CDS@users.noreply.github.com>
2022-05-03 13:38:36 +0100

    Minor changes to app settings and code

--- futurenhs.api/FutureNHS.Api/DataAccess/ContentApi/Providers/ContentApiClientProvider.cs
+++ futurenhs.api/FutureNHS.Api/DataAccess/ContentApi/Providers/ContentApiClientProvider.cs
@@ -5,48 +5,59 @@ namespace FutureNHS.Api.DataAccess.Repositories.ContentApi.ContentApiProvider
     public sealed class ContentApiClientProvider : IContentApiClientProvider
     {
         private readonly IConfiguration _configuration;
+        private readonly ILogger<ContentApiClientProvider> _logger;
 
-        public ContentApiClientProvider(IConfiguration configuration)
+        public ContentApiClientProvider(IConfiguration configuration, ILogger<ContentApiClientProvider> logger)
         {
             _configuration = configuration;
+            _logger = logger;
         }
 
         /// <inheritdoc />
         public async Task<T> SendRequestAsync<T>(HttpMethod method, string requestUrl, JsonContent? json = null)
         {
-            // REMOVES SSL - FOR DEVELOPMENT ONLY
-            var httpClientHandler = new HttpClientHandler();
-            httpClientHandler.ServerCertificateCustomValidationCallback = (
-                message,
-                cert,
-                chain,
-                sslPolicyErrors) =>
+            try
             {
-                return true;
-            };
-
-            var baseUrl = _configuration.GetValue<Uri>("ContentApi:ContentApiPUrl");
-            var absoluteUrl = new Uri(baseUrl + requestUrl);
-
-            using (var client = new HttpClient(httpClientHandler))
-            {
-                using var httpRequestMessage = new HttpRequestMessage(method, absoluteUrl)
+                // REMOVES SSL - FOR DEVELOPMENT ONLY
+                var httpClientHandler = new HttpClientHandler();
+                httpClientHandler.ServerCertificateCustomValidationCallback = (
+                    message,
+                    cert,
+                    chain,
+                    sslPolicyErrors) =>
                 {
-                    Content = json
+                    return true;
                 };
 
-                var response = await client
-                    .SendAsync(httpRequestMessage, HttpCompletionOption.ResponseHeadersRead)
-                    .ConfigureAwait(false);
+                var baseUrl = _configuration.GetValue<Uri>("ContentApi:ContentApiUrl");
+                var absoluteUrl = new Uri(baseUrl + requestUrl);
 
-                if (response.IsSuccessStatusCode)
+                using (var client = new HttpClient(httpClientHandler))
                 {
-                    var result = await response.Content.ReadFromJsonAsync<T>();
-                    return result;
+                    using var httpRequestMessage = new HttpRequestMessage(method, absoluteUrl)
+                    {
+                        Content = json
+                    };
+
+                    var response = await client
+                        .SendAsync(httpRequestMessage, HttpCompletionOption.ResponseHeadersRead)
+                        .ConfigureAwait(false);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var result = await response.Content.ReadFromJsonAsync<T>();
+                        return result;
+                    }
+
+                    return default(T);
                 }
 
-                return default(T);
-            };
+                ;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,$"Error connecting to content Api Client - {ex.Message}");
+            }
         }
     }
 }
