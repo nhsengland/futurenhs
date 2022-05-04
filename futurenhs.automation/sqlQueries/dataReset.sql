bf751490e1a5fb40fc11ee82753ea5120be4ab82
Alex Grubba <alex.grubba@cds.co.uk>
2022-05-04 11:32:07 +0100

    [*] refactored dataReset and dataTeardown scripts removing uneeded scripts, and hardcoded values for more generic Select statements to improve stability

--- futurenhs.automation/sqlQueries/dataReset.sql
+++ futurenhs.automation/sqlQueries/dataReset.sql
@@ -1,116 +1,123 @@
 BEGIN TRANSACTION 
 
     -- Disable constraints for all tables:
-    EXEC sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT all'
+    EXEC sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT all';
 
-DECLARE @autoAdmin AS uniqueidentifier;
-SELECT @autoAdmin = id FROM [dbo].[MembershipUser] WHERE Email = 'autoAdmin@test.co.uk'
-DECLARE @groupAdmin AS uniqueidentifier;
-SELECT @groupAdmin = id FROM [dbo].[MembershipUser] WHERE Email = 'autoGroupAdmin@test.co.uk'
-DECLARE @autoUser AS uniqueidentifier;
-SELECT @autoUser = id FROM [dbo].[MembershipUser] WHERE Email = 'autoUser@test.co.uk'
-DECLARE @autoUser2 AS uniqueidentifier;
-SELECT @autoUser2 = id FROM [dbo].[MembershipUser] WHERE Email = 'autoUser2@test.co.uk'
-DECLARE @autoUser3 AS uniqueidentifier;
-SELECT @autoUser3 = id FROM [dbo].[MembershipUser] WHERE Email = 'autoUser3@test.co.uk'
+	DECLARE @autoAdmin AS uniqueidentifier;
+	SELECT @autoAdmin = id FROM [dbo].[MembershipUser] WHERE Email = 'autoAdmin@test.co.uk';
+	DECLARE @groupAdmin AS uniqueidentifier;
+	SELECT @groupAdmin = id FROM [dbo].[MembershipUser] WHERE Email = 'autoGroupAdmin@test.co.uk';
+	DECLARE @autoUser AS uniqueidentifier;
+	SELECT @autoUser = id FROM [dbo].[MembershipUser] WHERE Email = 'autoUser@test.co.uk';
+	DECLARE @autoUser2 AS uniqueidentifier;
+	SELECT @autoUser2 = id FROM [dbo].[MembershipUser] WHERE Email = 'autoUser2@test.co.uk';
+	DECLARE @autoUser3 AS uniqueidentifier;
+	SELECT @autoUser3 = id FROM [dbo].[MembershipUser] WHERE Email = 'autoUser3@test.co.uk';
 
--- Delete posts made as part of forumSubmission.feature
-DECLARE @forumSubmissionTopic AS uniqueidentifier;
-SELECT @forumSubmissionTopic = Entity_Id FROM [dbo].[Discussion] WHERE Title = 'forumSubmission Discussion';
-DELETE [dbo].[Comment] WHERE [dbo].[Comment].Content LIKE '%Comment posted by the automation%' AND Parent_EntityId = @forumSubmissionTopic;
-DELETE [dbo].[Comment] WHERE [dbo].[Comment].Content LIKE '%This is a reply%' AND Parent_EntityId = @forumSubmissionTopic;
-DELETE [dbo].[Comment] WHERE [dbo].[Comment].Content LIKE '%This is another reply%' AND Parent_EntityId = @forumSubmissionTopic;
+	-- Table of all entityIds created by data submitted through the automation to be deleted
+	DECLARE @entityTable TABLE (id VARCHAR(50));
 
--- Delete discussions generated as part of forumAdmin.feature
-	 -- Create a new discussion 
-DECLARE @testDiscussion AS uniqueidentifier;
-SELECT @testDiscussion = Entity_Id FROM [dbo].[Discussion] WHERE Title = 'autoTestDiscussion';
-DELETE [dbo].[Comment] WHERE Parent_EntityId = @testDiscussion;
-DELETE [dbo].[Discussion] WHERE Entity_Id = @testDiscussion AND CreatedBy = @groupAdmin
+	-- Delete posts made as part of forumSubmission.feature
+	DECLARE @forumSubmissionTopic AS uniqueidentifier;
+	SELECT @forumSubmissionTopic = Entity_Id FROM [dbo].[Discussion] WHERE Title = 'forumSubmission Discussion';
+	INSERT INTO @entityTable (id) SELECT Entity_Id FROM [dbo].[Comment] WHERE Content LIKE '%Comment posted by the automation%' AND Parent_EntityId = @forumSubmissionTopic;
+	INSERT INTO @entityTable (id) SELECT Entity_Id FROM [dbo].[Comment] WHERE Content LIKE '%This is a reply%' AND Parent_EntityId = @forumSubmissionTopic;
+	INSERT INTO @entityTable (id) SELECT Entity_Id FROM [dbo].[Comment] WHERE Content LIKE '%This is another reply%' AND Parent_EntityId = @forumSubmissionTopic;
+	DELETE [dbo].[Comment] WHERE [dbo].[Comment].Content LIKE '%Comment posted by the automation%' AND Parent_EntityId = @forumSubmissionTopic;
+	DELETE [dbo].[Comment] WHERE [dbo].[Comment].Content LIKE '%This is a reply%' AND Parent_EntityId = @forumSubmissionTopic;
+	DELETE [dbo].[Comment] WHERE [dbo].[Comment].Content LIKE '%This is another reply%' AND Parent_EntityId = @forumSubmissionTopic;
 
-DECLARE @test100CharDiscussion AS uniqueidentifier;
-SELECT @test100CharDiscussion = Entity_Id FROM [dbo].[Discussion] WHERE Title = 'sWjuuYFV2o14Mk7RxWGAmpqO25feI8YkR5QhmiB1gTRTFzxg4xqylWDmit9scXIiQQyAYFwVYFbwxObPEBeTvLFPeWsb4GJI6tg3';
-DELETE [dbo].[Comment] WHERE Parent_EntityId = @test100CharDiscussion;
-DELETE [dbo].[Discussion] WHERE Entity_Id = @test100CharDiscussion AND CreatedBy = @groupAdmin
+	-- Delete discussions generated as part of forumAdmin.feature
+	DECLARE @automationDiscussions AS uniqueidentifier;
+	SELECT @automationDiscussions = Entity_Id FROM [dbo].[Discussion] WHERE CreatedBy = @groupAdmin;
+	INSERT INTO @entityTable (id) SELECT Entity_Id FROM [dbo].[Comment] WHERE CreatedBy = @groupAdmin;
+	INSERT INTO @entityTable (id) SELECT Entity_Id FROM [dbo].[Discussion] WHERE CreatedBy = @groupAdmin;
+	DELETE [dbo].[Comment] WHERE Parent_EntityId = @automationDiscussions;
+	DELETE [dbo].[Discussion] WHERE CreatedBy = @groupAdmin;
 
--- Delete Folder generated as part of filesManagement.feature
-DECLARE @adminGroup AS uniqueidentifier;
-SELECT @adminGroup = Id FROM [dbo].[Group] WHERE Name = 'Automation Admin Group'
-DELETE FROM [dbo].[Folder] WHERE Group_Id = @adminGroup AND Description = 'automation folder description' OR Title = 'VbNC63Shkzeyyp81jSxtQvrBf0kAFMiSt9LGe8ncL5v61wnVxPE2FIgIl36lIynd7BlCJA6DvgcH02zgLGtLySYghOahZGcyKGATqafAazK8uPph70gwk8sGEjL7XCTFy9POKDkRoFHM2nSTxipNe6vMNPcYGeD2uEC3cBqVkS2i39NYOkzPSP4F5nWhqQFnhZwZmw6E' 
-DELETE FROM [dbo].[Folder] WHERE Group_Id = @adminGroup AND IsDeleted = 1
+	-- Delete Folder generated as part of filesManagement.feature
+	DECLARE @adminGroup AS uniqueidentifier;
+	SELECT @adminGroup = Id FROM [dbo].[Group] WHERE Name = 'Automation Admin Group';
+	DELETE FROM [dbo].[Folder] WHERE Group_Id = @adminGroup AND CreatedBy = @groupAdmin;
+	DELETE FROM [dbo].[Folder] WHERE Group_Id = @adminGroup AND IsDeleted = 1;
 
--- Delete files uploaded as part of filesNavigation.feature
-DECLARE @rootFolder AS uniqueidentifier;
-SELECT @rootFolder = Id FROM [dbo].[Folder] WHERE title = 'Empty Folder' AND IsDeleted = 0
-DELETE [dbo].[File] WHERE [ParentFolder] = @rootFolder
+	-- Delete files uploaded as part of filesNavigation.feature
+	DECLARE @rootFolder AS uniqueidentifier;
+	SELECT @rootFolder = Id FROM [dbo].[Folder] WHERE title = 'Empty Folder' AND IsDeleted = 0;
+	DELETE [dbo].[File] WHERE [ParentFolder] = @rootFolder;
 
--- Reset Users to Pending Group Access groupAdmin.feature
-	-- Approve group member request
-	-- Reject group member request
-UPDATE [dbo].[GroupUser] SET Approved = 0, ApprovedToJoinDateUTC = NULL, ApprovingMembershipUser_Id = NULL WHERE MembershipUser_Id = @autoUser2 AND Group_Id = @adminGroup;
-UPDATE [dbo].[GroupUser] SET Rejected = 0, ApprovedToJoinDateUTC = NULL, ApprovingMembershipUser_Id = NULL WHERE MembershipUser_Id = @autoUser3 AND Group_Id = @adminGroup;
+	-- Reset Users to Pending Group Access groupAdmin.feature
+		-- Approve group member request
+		-- Reject group member request
+	UPDATE [dbo].[GroupUser] SET Approved = 0, ApprovedToJoinDateUTC = NULL, ApprovingMembershipUser_Id = NULL WHERE MembershipUser_Id = @autoUser2 AND Group_Id = @adminGroup;
+	UPDATE [dbo].[GroupUser] SET Rejected = 0, ApprovedToJoinDateUTC = NULL, ApprovingMembershipUser_Id = NULL WHERE MembershipUser_Id = @autoUser3 AND Group_Id = @adminGroup;
 
--- Remove users from groups added as part of automation
-	-- Add registered user
-DECLARE @publicGroup AS uniqueidentifier;
-SELECT @publicGroup = Id FROM [dbo].[Group] WHERE Name = 'Automation Public Group'
-DELETE FROM [dbo].[GroupUser] WHERE MembershipUser_Id = @autoUser2 AND Group_Id = @publicGroup
+	-- Remove users from groups added as part of automation
+		-- Add registered user
+	DECLARE @publicGroup AS uniqueidentifier;
+	SELECT @publicGroup = Id FROM [dbo].[Group] WHERE Name = 'Automation Public Group';
+	DELETE FROM [dbo].[GroupUser] WHERE MembershipUser_Id = @autoUser2 AND Group_Id = @publicGroup;
 
-	-- Join a private group
-DECLARE @privateGroup AS uniqueidentifier;
-SELECT @privateGroup = Id FROM [dbo].[Group] WHERE Name = 'Automation Private Group'
-DELETE FROM [dbo].[GroupUser] WHERE MembershipUser_Id = @autoUser AND Group_Id = @privateGroup
+		-- Join a private group
+	DECLARE @privateGroup AS uniqueidentifier;
+	SELECT @privateGroup = Id FROM [dbo].[Group] WHERE Name = 'Automation Private Group';
+	DELETE FROM [dbo].[GroupUser] WHERE MembershipUser_Id = @autoUser AND Group_Id = @privateGroup;
 
---Delete new user details generated
-	-- Invite User Form
-DELETE [dbo].[GroupInvite] WHERE EmailAddress = 'autoTest@email.com'
-	-- New user registration
-DELETE [dbo].[MembershipUser] WHERE UserName = 'autoTest@email.com'
+	--Delete new user details generated
+		-- Invite User Form
+	DELETE [dbo].[GroupInvite] WHERE EmailAddress = 'autoTest@email.com';
+		-- New user registration
+	DELETE [dbo].[MembershipUser] WHERE UserName = 'autoTest@email.com';
 
---Delete new group generated
-	-- Group creation new fields	
-DECLARE @groupid AS uniqueidentifier;
-SELECT @groupid = id FROM [dbo].[Group] WHERE [Name] = 'Automation Created Group';
+	--Delete new group generated
+		-- Group creation new fields	
+	DECLARE @groupid AS uniqueidentifier;
+	SELECT @groupid = id FROM [dbo].[Group] WHERE [Name] = 'Automation Created Group';
 
-DELETE [dbo].[GroupUser] WHERE Group_Id = @groupid
-DELETE [dbo].[Group] WHERE Id = @groupid
+	DELETE [dbo].[GroupUser] WHERE Group_Id = @groupid;
+	DELETE [dbo].[Group] WHERE Id = @groupid;
 
--- Reset changes made to Automation Editable Group information
-DECLARE @editgroup AS uniqueidentifier;
-SELECT @editgroup = id FROM [dbo].[Group] WHERE [Name] = 'Automation Edited Group';
-UPDATE [dbo].[Group] SET Name = 'Automation Editable Group',
-						 Description = 'DO NOT USE - This group is reserved solely for use by our automated test scripts', 
-						 Subtitle = 'DO NOT USE - This group is reserved solely for use by our automated test scripts', 
-		 				 PublicGroup = 0, 
-		 				 Introduction = 'Introduction ',
-						 ImageId = NULL,
-		 				 AboutUs = NULL
-					WHERE Id = @editgroup
+	-- Reset changes made to Automation Editable Group information
+	DECLARE @editgroup AS uniqueidentifier;
+	SELECT @editgroup = id FROM [dbo].[Group] WHERE [Name] = 'Automation Edited Group';
+	UPDATE [dbo].[Group] SET 
+		Name = 'Automation Editable Group',
+		Description = 'DO NOT USE - This group is reserved solely for use by our automated test scripts', 
+		Subtitle = 'DO NOT USE - This group is reserved solely for use by our automated test scripts', 
+		PublicGroup = 0, 
+		Introduction = 'Introduction ',
+		ImageId = NULL,
+		AboutUs = NULL
+	WHERE Id = @editgroup;
 
--- Reset changes made to autoEditUser in edit profile tests
-DECLARE @editUser AS uniqueidentifier;
-SELECT @editUser = id FROM [dbo].[MembershipUser] WHERE Email = 'autoEditUser@test.co.uk'
-UPDATE [dbo].[MembershipUser] SET FirstName = 'autoEdit', 
-								  Surname = 'User',
-								  Initials = 'AU',
-								  Pronouns = ''
-							  WHERE Id = @editUser
+	-- Reset changes made to autoEditUser in edit profile tests
+	DECLARE @editUser AS uniqueidentifier;
+	SELECT @editUser = id FROM [dbo].[MembershipUser] WHERE Email = 'autoEditUser@test.co.uk';
+	UPDATE [dbo].[MembershipUser] SET 
+		FirstName = 'autoEdit', 
+		Surname = 'User',
+		Initials = 'AU',
+		Pronouns = ''
+	WHERE Id = @editUser;
 
--- Reset changes made to docTest file in Automation Test Folder
-DECLARE @docTest AS uniqueidentifier
-SELECT @docTest = id FROM [dbo].[File] WHERE Title = 'Doc Test'
-UPDATE [dbo].[File] SET Title = 'docTest', Description = 'test doc' WHERE Id = @docTest
+	-- Reset changes made to docTest file in Automation Test Folder
+	DECLARE @docTest AS uniqueidentifier;
+	SELECT @docTest = id FROM [dbo].[File] WHERE Title = 'Doc Test';
+	UPDATE [dbo].[File] SET Title = 'docTest', Description = 'test doc' WHERE Id = @docTest;
 
--- Reset changes made to DeleteFolder 
-DECLARE @deletefolder AS uniqueidentifier
-SELECT @deletefolder = id FROM [dbo].[Folder] WHERE Title = 'DeleteFolder'
-UPDATE [dbo].[Folder] SET IsDeleted = 0 WHERE Id = @deletefolder
+	-- Reset changes made to DeleteFolder 
+	DECLARE @deletefolder AS uniqueidentifier;
+	SELECT @deletefolder = id FROM [dbo].[Folder] WHERE Title = 'DeleteFolder';
+	UPDATE [dbo].[Folder] SET IsDeleted = 0 WHERE Id = @deletefolder;
 
--- Reset changes made to EditableFolder 
-DECLARE @editfolder AS uniqueidentifier
-SELECT @editfolder = id FROM [dbo].[Folder] WHERE Title = 'EditedFolder'
-UPDATE [dbo].[Folder] SET Title = 'EditableFolder' WHERE Id = @editfolder
+	-- Reset changes made to EditableFolder 
+	DECLARE @editfolder AS uniqueidentifier;
+	SELECT @editfolder = id FROM [dbo].[Folder] WHERE Title = 'EditedFolder';
+	UPDATE [dbo].[Folder] SET Title = 'EditableFolder' WHERE Id = @editfolder;
 	
+	-- Delete all values in Entity Table from submitted data
+	DELETE FROM [dbo].[Entity] WHERE [Id] IN (SELECT * FROM @entityTable);
+
     -- Re-enable constraints for all tables:
     EXEC sp_msforeachtable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all';	
 
--- futurenhs.automation/sqlQueries/fullAutomationDataTeardown.sql
+++ futurenhs.automation/sqlQueries/fullAutomationDataTeardown.sql
@@ -4,7 +4,11 @@ BEGIN TRANSACTION
     EXEC sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT all'
 
     SELECT [Id] INTO #autoUsers FROM [dbo].[MembershipUser] WHERE [Email] IN ('autoAdmin@test.co.uk', 'autoGroupAdmin@test.co.uk', 'autoUser@test.co.uk', 'autoUser2@test.co.uk', 'autoUser3@test.co.uk', 'autoTest@email.com', 'VisRegUser@email.com', 'autoEditUser@test.co.uk');
-	
+	DECLARE @groupAdmin AS uniqueidentifier;
+    SELECT @groupAdmin = id FROM [dbo].[MembershipUser] WHERE Email = 'autoGroupAdmin@test.co.uk';
+    DECLARE @autoUser AS uniqueidentifier;
+    SELECT @autoUser = id FROM [dbo].[MembershipUser] WHERE Email = 'autoUser@test.co.uk';
+
 	DECLARE @entityTable TABLE (id VARCHAR(50))
 	INSERT INTO @entityTable ([Id]) VALUES ((SELECT Entity_Id FROM Comment WHERE Content ='<p>First Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'))
 	INSERT INTO @entityTable ([Id]) VALUES ((SELECT Entity_Id FROM Comment WHERE Content ='<p>Third Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'))
@@ -19,7 +23,9 @@ BEGIN TRANSACTION
 	INSERT INTO @entityTable VALUES (N'ca5c5162-7a2a-482e-b15b-ad7f010e30cf'), (N'edcc5437-b704-45d8-8972-ad7f010e2316'), (N'e56abf3c-7aa0-465a-a146-ad7f010e4acc'), (N'bcca8784-0e5e-496c-9bbb-ad7f010e51b4'), (N'e846a7bc-327a-4c05-89c7-ad7f010e5ed2'), (N'e4940ced-087d-41e9-a350-ad7f010e6bc9'), (N'69f7c324-3c63-4917-8b22-ad7f010e719e'), (N'58ab24e5-f3f9-4f8d-aba9-ad7f010e809d'), (N'107d1f85-3366-410c-8ffe-ad7f010e8586'), (N'0e807603-6d32-4f4f-833b-ad7f010e903a'), (N'e28ed14b-46ae-4fdd-8988-ad7f010e959c'), (N'80d3fc63-4852-4163-98ac-ad880118903c'), 
 	(N'5313414a-ac9d-49eb-af90-ad880118b1ca'), (N'b578b2e5-cce4-4e84-9e64-ad880118bce2'), (N'c30117fc-dbb5-453c-9968-ad880118c70e'), (N'1af57d07-9012-4511-b9fc-ad88012033b0'), (N'4760e89f-048e-4b47-a70e-339cd43a2703'), (N'3cb17bfa-9ad4-4ab7-9801-36aff0278be0'), (N'5ca2c29e-31d6-448e-a409-6fbf83cbf844'), (N'9652f206-bd13-4920-b6d4-76858a21029a'), (N'13fa753a-fcba-4bf3-ae4c-771103503246'), (N'c0011070-c541-450e-a19f-7b4f91640835'), (N'285a2aa3-bfb8-41e1-b4d1-9349a68f2252'), (N'ce818d68-d4c5-4518-8b4f-982930b5f8c1'), (N'7120144f-a4a3-40dc-bd83-b7f8e7f8f66d'), 
 	(N'468cf575-9dcd-4897-9976-f747009dc13d'), (N'4654a062-b693-42f5-9b25-c8a286fa8ba9'), (N'493a2f0e-4195-4730-9f3c-ad7f010d40c8'), (N'3fbc0d4e-c2a7-46a0-99e9-ad7f010eb4ad'), (N'a4bf9b51-d145-4601-92b6-ad7f010ebe84'), (N'6ea66f95-24bc-4179-94f4-ad7f010ec730'), (N'a6da1550-066f-4ae6-ae98-ad7f010ece99'), (N'e1d477d3-dfb0-4acd-b74d-ad7f010ed722'), (N'55f516e0-9117-4a43-9363-ad8200ce7d1a'), (N'96b155ba-e8de-40e3-980e-ad8800b0a6b8'), (N'83702899-fdbf-49ed-bb1d-adda00f757c3'), (N'5889c069-5061-403b-93f6-adda00f7638a'), (N'f91c0e75-9d3d-4700-9efb-adda00f770d0'), (N'eaf7a8f1-b9a0-415d-b26d-88517132536e')
-    SELECT * FROM @entityTable
+    INSERT INTO @entityTable ([id]) SELECT Entity_Id FROM [dbo].[Comment] WHERE CreatedBy = @groupAdmin;
+    INSERT INTO @entityTable ([id]) SELECT Entity_Id FROM [dbo].[Comment] WHERE CreatedBy = @autoUser;
+    INSERT INTO @entityTable ([id]) SELECT Entity_Id FROM [dbo].[Discussion] WHERE CreatedBy = @groupAdmin;
 	
 	DELETE FROM [dbo].[MembershipUser]
     WHERE [Id] IN (
@@ -75,72 +81,6 @@ BEGIN TRANSACTION
     WHERE [Id] IN (
         SELECT * FROM @entityTable
     );
-
-    -- Delete posts made as part of forumSubmission.feature
-    DECLARE @forumSubmissionTopic AS uniqueidentifier;
-    SELECT @forumSubmissionTopic = Entity_Id FROM [dbo].[Discussion] WHERE Title = 'forumSubmission Discussion';
-    DELETE [dbo].[Comment] WHERE [dbo].[Comment].Content LIKE '%Comment posted by the automation%' AND Parent_EntityId = @forumSubmissionTopic;
-    DELETE [dbo].[Comment] WHERE [dbo].[Comment].Content LIKE '%This is a reply%' AND Parent_EntityId = @forumSubmissionTopic;
-    DELETE [dbo].[Comment] WHERE [dbo].[Comment].Content LIKE '%This is another reply%' AND Parent_EntityId = @forumSubmissionTopic;
-
-    -- Delete Folder generated as part of filesManagement.feature
-    DECLARE @adminGroup AS uniqueidentifier;
-    SELECT @adminGroup = Id FROM [dbo].[Group] WHERE Name = 'Automation Admin Group'
-    DELETE FROM [dbo].[Folder] WHERE Group_Id = @adminGroup AND Title != 'Empty Folder' AND Title != 'Automation Test Folder'
-    DELETE FROM [dbo].[Folder] WHERE Group_Id = @adminGroup AND IsDeleted = 1
-
-    -- Delete files uploaded as part of filesNavigation.feature
-    DECLARE @rootFolder AS uniqueidentifier;
-    SELECT @rootFolder = Id FROM [dbo].[Folder] WHERE Title = 'Empty Folder' AND IsDeleted = 0
-    DELETE [dbo].[File] WHERE [ParentFolder] = @rootFolder
-
-    --Delete new user details generated
-        -- Invite User Form
-    DELETE [dbo].[GroupInvite] WHERE EmailAddress = 'autoTest@email.com'
-
-	--Delete new group generated
-		-- Group creation new fields	
-	DECLARE @groupid AS uniqueidentifier;
-	SELECT @groupid = id FROM [dbo].[Group] WHERE [Name] = 'Automation Created Group';
-
-	DELETE [dbo].[GroupUser] WHERE Group_Id = @groupid
-	DELETE [dbo].[Group] WHERE Id = @groupid
-
-	-- Reset changes made to Automation Editable Group information
-	DECLARE @editgroup AS uniqueidentifier;
-	SELECT @editgroup = id FROM [dbo].[Group] WHERE [Name] = 'Automation Edited Group';
-	UPDATE [dbo].[Group] SET Name = 'Automation Editable Group',
-							 Description = 'DO NOT USE - This group is reserved solely for use by our automated test scripts', 
-							 Subtitle = 'DO NOT USE - This group is reserved solely for use by our automated test scripts', 
-		 					 PublicGroup = 0, 
-		 					 Introduction = 'Introduction ',
-							 ImageId = NULL,
-		 					 AboutUs = NULL
-						WHERE Id = @editgroup
-
-	-- Reset changes made to autoEditUser in edit profile tests
-	DECLARE @editUser AS uniqueidentifier;
-	SELECT @editUser = id FROM [dbo].[MembershipUser] WHERE Email = 'autoEditUser@test.co.uk'
-	UPDATE [dbo].[MembershipUser] SET FirstName = 'autoEdit', 
-									  Surname = 'User',
-									  Initials = 'AU',
-									  Pronouns = ''
-								  WHERE Id = @editUser
-
-	-- Reset changes made to docTest file in Automation Test Folder
-	DECLARE @docTest AS uniqueidentifier
-	SELECT @docTest = id FROM [dbo].[File] WHERE Title = 'Doc Test'
-	UPDATE [dbo].[File] SET Title = 'docTest', Description = 'test doc' WHERE Id = @docTest
-
-	-- Reset changes made to DeleteFolder 
-	DECLARE @deletefolder AS uniqueidentifier
-	SELECT @deletefolder = id FROM [dbo].[Folder] WHERE Title = 'DeleteFolder'
-	UPDATE [dbo].[Folder] SET IsDeleted = 0 WHERE Id = @deletefolder
-
-	-- Reset changes made to EditableFolder 
-	DECLARE @editfolder AS uniqueidentifier
-	SELECT @editfolder = id FROM [dbo].[Folder] WHERE Title = 'EditedFolder'
-	UPDATE [dbo].[Folder] SET Title = 'EditableFolder' WHERE Id = @editfolder
 
 	DROP TABLE #autoUsers
 
