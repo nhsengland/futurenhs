c5b1ce6a6b960a25549b70c38ccab943a24b2e95
Mathew Beldon <mathew.beldon@cds.co.uk>
2022-05-09 17:01:33 +0100

    [*] Updates edit group user role endpoints to take MembershipUserId

---
+++ futurenhs.api/FutureNHS.Api/Models/Group/Request/UpdateGroupUserRoleRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace FutureNHS.Api.Models.Group.Request
+{
+    public sealed class UpdateGroupUserRoleRequest
+    {
+        public Guid GroupUserRoleId { get; init; }
+    }
+}
--- futurenhs.api/FutureNHS.Api/Services/Interfaces/IGroupMembershipService.cs
+++ futurenhs.api/FutureNHS.Api/Services/Interfaces/IGroupMembershipService.cs
@@ -7,9 +7,9 @@ namespace FutureNHS.Api.Services.Interfaces
     {
         Task UserJoinGroupAsync(Guid userId, string slug, CancellationToken cancellationToken);
         Task UserLeaveGroupAsync(Guid userId, string slug, CancellationToken cancellationToken);
-        Task<GroupMemberDetails> GetGroupMembershipUserAsync(Guid userId, Guid groupUserId, string slug, CancellationToken cancellationToken);
+        Task<GroupMemberDetails> GetGroupMembershipUserAsync(Guid userId, Guid targetUserId, string slug, CancellationToken cancellationToken);
         Task<IEnumerable<GroupRoleDto>> GetMembershipRolesForGroupAsync(Guid userId, string slug, CancellationToken cancellationToken); 
-        Task UpdateGroupMembershipUserRoleAsync(Guid userId, string slug, Guid groupUserId, Guid groupRoleId, byte[] rowVersion, CancellationToken cancellationToken);
-        Task DeleteGroupMembershipUserAsync(Guid userId, string slug, Guid groupUserId, byte[] rowVersion, CancellationToken cancellationToken);
+        Task UpdateGroupMembershipUserRoleAsync(Guid userId, string slug, Guid targetUserId, Guid groupRoleId, byte[] rowVersion, CancellationToken cancellationToken);
+        Task DeleteGroupMembershipUserAsync(Guid userId, string slug, Guid targetUserId, byte[] rowVersion, CancellationToken cancellationToken);
     }
 }
