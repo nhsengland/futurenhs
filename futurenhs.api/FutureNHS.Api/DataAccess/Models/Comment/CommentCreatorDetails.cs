77c8d190b18531adef5914f5e05410cee95e0ec5
Mathew Beldon <mathew.beldon@cds.co.uk>
2022-05-18 10:44:39 +0100

    [API] Story 94953 - Notifications - Reply to a comment
    [+] Adds notifications for a users reply on a comment

---
+++ futurenhs.api/FutureNHS.Api/DataAccess/Models/Comment/CommentCreatorDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace FutureNHS.Api.DataAccess.Models.Comment
+{
+    public class CommentCreatorDetails
+    {
+        public Guid CommentId { get; init; }
+        public Guid DiscussionId { get; init; }
+        public string GroupSlug { get; init; }
+        public string? CreatedAtUtc { get; init; }
+        public Guid CreatedById { get; init; }
+        public string CreatedByName { get; init; }
+        public string CreatedByEmail { get; init; }
+        public string CreatedBySlug { get; init; }
+    }
+}
--- futurenhs.api/FutureNHS.Api/Services/Notifications/Interfaces/ICommentNotificationService.cs
+++ futurenhs.api/FutureNHS.Api/Services/Notifications/Interfaces/ICommentNotificationService.cs
@@ -3,5 +3,6 @@
     public interface ICommentNotificationService
     {
         Task SendNotificationToDiscussionCreatorAsync(Guid posterId, Guid discussionId, CancellationToken cancellationToken);
+        Task SendNotificationToCommentCreatorAsync(Guid posterId, Guid commentId, CancellationToken cancellationToken);
     }
 }
