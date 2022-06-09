ca8da9da2c678cd1b0991af2b5f77406fb5a877e
ReallySmall <richard.iles@digital.cds.uk>
2022-06-09 12:46:28 +0100

    [*] Progresses Loader component, delays hiding of spinner and adds fade out animation

--- futurenhs.app/components/Loader/index.tsx
+++ futurenhs.app/components/Loader/index.tsx
@@ -6,18 +6,24 @@ import { Spinner } from '@components/Spinner';
 import { Props } from './interfaces'
 
 export const Loader: (props: Props) => JSX.Element = ({
+    isActive,
     text,
     delay = 0,
+    remain = 0,
     className,
 }) => {
 
     const [shouldRender, setShouldRender] = useState(false);
+    const [isEnding, setIsEnding] = useState(false);
     const loadingTimeOut: any = useRef(null);
+    const endingTimeOut: any = useRef(null)
 
     const { loadingMessage } = text ?? {};
 
     const generatedClasses: any = {
-        wrapper: classNames('c-loader', className),
+        wrapper: classNames('c-loader', className, {
+            [`u-transition-opacity u-duration-750 u-ease-out u-opacity-0`]: isEnding
+        }),
         content: classNames('c-loader_content'),
         message: classNames('c-loader_message'),
         spinner: classNames('c-loader_spinner')
@@ -25,32 +31,43 @@ export const Loader: (props: Props) => JSX.Element = ({
 
     const renderAfterDelay = (): void => {
 
+        window.clearTimeout(endingTimeOut.current)
+        setIsEnding(false)
+
         loadingTimeOut.current = window.setTimeout(() => {
 
-            setShouldRender(true);
-
+            isActive && setShouldRender(true);
+            
         }, delay);
+
+    }
+
+
+    const hideAfterRemain = (): void => {
+
+        setIsEnding(true)
+
+        endingTimeOut.current = window.setTimeout(() => {
+
+            setShouldRender(false)
+
+        }, remain);
 
     }
 
     useEffect(() => {
 
-        renderAfterDelay();
+        isActive ? renderAfterDelay() : hideAfterRemain()
 
         return () => {
 
             window.clearTimeout(loadingTimeOut.current);
+            window.clearTimeout(endingTimeOut.current);
 
         }
 
-    }, []);
+    }, [isActive]);
 
-    useEffect(() => {
-
-        setShouldRender(false);
-        renderAfterDelay();
-
-    }, [delay]);
 
     if(shouldRender){
 
