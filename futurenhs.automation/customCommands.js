cb64c922356734528908b6b32ccad015af5da61d
Alex Grubba <alex.grubba@cds.co.uk>
2022-06-15 14:19:22 +0100

    [+] adding fedtest run customcommands for pipeline use

--- futurenhs.automation/customCommands.js
+++ futurenhs.automation/customCommands.js
@@ -4,6 +4,8 @@
         "core": `wdio wdio.headless.conf.js --suite=smokeTest --cucumberOpts.tagExpression="@Core and not @Pending and not @NotLocal"`,
         "local": `wdio wdio.headless.conf.js --baseUrl=http://localhost:5000/ --suite=fullRegression --cucumberOpts.tagExpression="not @Pending and not @NotInLocal"`,
         "devTest": `wdio wdio.headless.conf.js --baseUrl=https://collaborate-dev.future.nhs.uk/ --suite=fullRegression`,
+        "devTestAxe": `npm run scenario wdio.headless.conf.js Url=https://collaborate-dev.future.nhs.uk/ FNHS:FED02`,
+        "devTestLighthouse": `run scenario wdio.headless.conf.js Url=https://collaborate-dev.future.nhs.uk/ FNHS:FED01`,
         "uat": `wdio wdio.headless.conf.js --baseUrl=https://collaborate-uat.future.nhs.uk/ --suite=fullRegression`,
         "firefox": `wdio wdio.firefox.conf.js --suite=fullRegression`,
         "msedge": `wdio wdio.msedge.conf.js --suite=fullRegression`,
