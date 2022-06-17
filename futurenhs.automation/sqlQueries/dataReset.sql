3856e5b4edfc51a4c57bb5d4fe37d24a0772541a
Alex Grubba <alex.grubba@cds.co.uk>
2022-06-17 16:41:55 +0100

    [*] improved groupUser teardown handling in teardown [*] reduced data management needs for Entity IDs in Teardown and Seed

--- futurenhs.automation/sqlQueries/dataReset.sql
+++ futurenhs.automation/sqlQueries/dataReset.sql
@@ -11,22 +11,8 @@ BEGIN
     SELECT @autoUser = id FROM [dbo].[MembershipUser] WHERE Email = 'autoUser@test.co.uk';
 
 	DECLARE @entityTable TABLE (id VARCHAR(50))
-	INSERT INTO @entityTable SELECT Entity_Id FROM Comment WHERE Content ='<p>First Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'
-	INSERT INTO @entityTable SELECT Entity_Id FROM Comment WHERE Content ='<p>Third Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'
-	INSERT INTO @entityTable SELECT Entity_Id FROM Comment WHERE Content ='<p>Fourth Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'
-	INSERT INTO @entityTable SELECT Entity_Id FROM Comment WHERE Content ='<p>Fifth Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'
-	INSERT INTO @entityTable SELECT Entity_Id FROM Comment WHERE Content ='<p>Sixth Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'
-	INSERT INTO @entityTable SELECT Entity_Id FROM Comment WHERE Content ='<p>Seventh Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'
-	INSERT INTO @entityTable SELECT Entity_Id FROM Comment WHERE Content ='<p>Eighth Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'
-	INSERT INTO @entityTable SELECT Entity_Id FROM Comment WHERE Content ='<p>Ninth Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'
-	INSERT INTO @entityTable SELECT Entity_Id FROM Comment WHERE Content ='<p>Tenth Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'
-	INSERT INTO @entityTable SELECT Entity_Id FROM Comment WHERE Content ='<p>Eleventh Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'
-	INSERT INTO @entityTable VALUES (N'ca5c5162-7a2a-482e-b15b-ad7f010e30cf'), (N'edcc5437-b704-45d8-8972-ad7f010e2316'), (N'e56abf3c-7aa0-465a-a146-ad7f010e4acc'), (N'bcca8784-0e5e-496c-9bbb-ad7f010e51b4'), (N'e846a7bc-327a-4c05-89c7-ad7f010e5ed2'), (N'e4940ced-087d-41e9-a350-ad7f010e6bc9'), (N'69f7c324-3c63-4917-8b22-ad7f010e719e'), (N'58ab24e5-f3f9-4f8d-aba9-ad7f010e809d'), (N'107d1f85-3366-410c-8ffe-ad7f010e8586'), (N'0e807603-6d32-4f4f-833b-ad7f010e903a'), (N'e28ed14b-46ae-4fdd-8988-ad7f010e959c'), (N'80d3fc63-4852-4163-98ac-ad880118903c'), 
-	(N'5313414a-ac9d-49eb-af90-ad880118b1ca'), (N'b578b2e5-cce4-4e84-9e64-ad880118bce2'), (N'c30117fc-dbb5-453c-9968-ad880118c70e'), (N'1af57d07-9012-4511-b9fc-ad88012033b0'), (N'4760e89f-048e-4b47-a70e-339cd43a2703'), (N'3cb17bfa-9ad4-4ab7-9801-36aff0278be0'), (N'5ca2c29e-31d6-448e-a409-6fbf83cbf844'), (N'9652f206-bd13-4920-b6d4-76858a21029a'), (N'13fa753a-fcba-4bf3-ae4c-771103503246'), (N'c0011070-c541-450e-a19f-7b4f91640835'), (N'285a2aa3-bfb8-41e1-b4d1-9349a68f2252'), (N'ce818d68-d4c5-4518-8b4f-982930b5f8c1'), (N'7120144f-a4a3-40dc-bd83-b7f8e7f8f66d'), 
-	(N'468cf575-9dcd-4897-9976-f747009dc13d'), (N'4654a062-b693-42f5-9b25-c8a286fa8ba9'), (N'493a2f0e-4195-4730-9f3c-ad7f010d40c8'), (N'3fbc0d4e-c2a7-46a0-99e9-ad7f010eb4ad'), (N'a4bf9b51-d145-4601-92b6-ad7f010ebe84'), (N'6ea66f95-24bc-4179-94f4-ad7f010ec730'), (N'a6da1550-066f-4ae6-ae98-ad7f010ece99'), (N'e1d477d3-dfb0-4acd-b74d-ad7f010ed722'), (N'55f516e0-9117-4a43-9363-ad8200ce7d1a'), (N'96b155ba-e8de-40e3-980e-ad8800b0a6b8'), (N'83702899-fdbf-49ed-bb1d-adda00f757c3'), (N'5889c069-5061-403b-93f6-adda00f7638a'), (N'f91c0e75-9d3d-4700-9efb-adda00f770d0'), (N'eaf7a8f1-b9a0-415d-b26d-88517132536e')
-    INSERT INTO @entityTable SELECT Entity_Id FROM [dbo].[Comment] WHERE CreatedBy = @groupAdmin;
-    INSERT INTO @entityTable SELECT Entity_Id FROM [dbo].[Comment] WHERE CreatedBy = @autoUser;
-    INSERT INTO @entityTable SELECT Entity_Id FROM [dbo].[Discussion] WHERE CreatedBy = @groupAdmin;
+	INSERT INTO @entityTable SELECT Entity_Id FROM [dbo].[Comment] WHERE CreatedBy IN ( SELECT * FROM #autoUsers);
+    INSERT INTO @entityTable SELECT Entity_Id FROM [dbo].[Discussion] WHERE CreatedBy IN ( SELECT * FROM #autoUsers);
 	
     DELETE FROM [dbo].[GroupSite] 
 	WHERE [GroupId] IN (
@@ -57,8 +43,8 @@ BEGIN
     );
 
     DELETE FROM [dbo].[GroupUser] 
-    WHERE [MembershipUser_Id] IN (
-        SELECT * FROM #autoUsers
+    WHERE [Group_Id] IN (
+        SELECT [Id] FROM [dbo].[Group] WHERE [GroupOwner] IN (SELECT * FROM #autoUsers)
     );
 
     DELETE FROM [dbo].[MembershipUsersInRoles]
@@ -114,6 +100,8 @@ BEGIN
 	INSERT [dbo].[MembershipUser] ([Id], [UserName], [Password], [PasswordSalt], [Email], [IsApproved], [IsLockedOut], [IsBanned], [CreatedAtUTC], [LastLoginDateUTC], [LastPasswordChangedDateUTC], [LastLockoutDateUTC], [LastActivityDateUTC], [FailedPasswordAttemptCount], [FailedPasswordAnswerAttempt], [Slug], [HasAgreedToTermsAndConditions], [IsTrustedUser], [FirstName], [Surname], [Initials]) VALUES (N'cb72fa31-e1b2-428a-8847-ad57009babd9', N'autoUser3@test.co.uk', N'Cd6U3nRQDqEbHa/V26rxOxyIL28wqfAjPdYzfN0JylU=', N'2gbwL74MNaZ8QK4dcaiedfP/fdJwdIpA', N'autoUser3@test.co.uk', 1, 0, 0, CAST(N'2021-06-30 09:26:46.923' AS DateTime2), CAST(N'2021-08-31 15:59:51.490' AS DateTime2), CAST(N'2021-06-30 09:26:46.923' AS DateTime2), CAST(N'1753-01-01 00:00:00.000' AS DateTime2), CAST(N'2021-08-31 15:59:51.873' AS DateTime2), 0, 0, N'autouser3', 1, 1, N'Auto', N'User3', N'AU')
 	INSERT [dbo].[MembershipUser] ([Id], [UserName], [Password], [PasswordSalt], [Email], [IsApproved], [IsLockedOut], [IsBanned], [CreatedAtUTC], [LastLoginDateUTC], [LastPasswordChangedDateUTC], [LastLockoutDateUTC], [LastActivityDateUTC], [FailedPasswordAttemptCount], [FailedPasswordAnswerAttempt], [Slug], [HasAgreedToTermsAndConditions], [IsTrustedUser], [FirstName], [Surname], [Initials]) VALUES (N'a51c9076-cf85-42da-bb30-adda00f5b3c6', N'VisRegUser@email.com', N'wJuXPg5bhhTfTEaUVmSAanZvLvRQvi0eY7lhbv7UoS8=', N'e0ZQHTFeBgp5Bte3hq1QbXxkZv9FBULO', N'VisRegUser@email.com', 1, 0, 0, CAST(N'2021-11-08 14:54:34.483' AS DateTime2), CAST(N'2022-01-11 10:16:20.327' AS DateTime2), CAST(N'2021-11-08 14:54:34.483' AS DateTime2), CAST(N'1753-01-01 00:00:00.000' AS DateTime2), CAST(N'2022-01-11 10:16:20.567' AS DateTime2), 0, 0, N'visreguser-email-com', 1, 1, N'Vis', N'Reg', N'VR')
 	INSERT [dbo].[MembershipUser] ([Id], [UserName], [Password], [PasswordSalt], [Email], [IsApproved], [IsLockedOut], [IsBanned], [CreatedAtUTC], [LastLoginDateUTC], [LastPasswordChangedDateUTC], [LastLockoutDateUTC], [LastActivityDateUTC], [FailedPasswordAttemptCount], [FailedPasswordAnswerAttempt], [Slug], [HasAgreedToTermsAndConditions], [IsTrustedUser], [FirstName], [Surname], [Initials]) VALUES (N'24949588-81c0-4ae0-8d67-ae04011f41b4', N'autoEditUser@test.co.uk', N'GoHbGkQpGB/jgY3jN0hb5jSl4DEwbuME4dsdRP41NJw=', N'ZBp81AWhmQ2Qar5zuh6mmVPF/Fl5J6gg', N'autoEditUser@test.co.uk', 1, 0, 0, CAST(N'2021-12-20 17:25:52.167' AS DateTime2), CAST(N'2022-01-11 12:23:55.377' AS DateTime2), CAST(N'2021-12-20 17:25:52.167' AS DateTime2), CAST(N'1753-01-01 00:00:00.000' AS DateTime2), CAST(N'2022-01-11 10:43:43.793' AS DateTime2), 0, 0, N'autoedituser-test-co-uk', 1, 1, N'autoEdit', N'User', N'AU')	
+	
+	SELECT [Id] INTO #seededUsers FROM [dbo].[MembershipUser] WHERE [Email] IN ('autoAdmin@test.co.uk', 'autoGroupAdmin@test.co.uk', 'autoRemoveUser@test.co.uk', 'autoUser@test.co.uk', 'autoUser2@test.co.uk', 'autoUser3@test.co.uk', 'autoTest@email.com', 'VisRegUser@email.com', 'autoEditUser@test.co.uk');
 	
 	/**
 	* INSERT AUTOMATION FILES
@@ -198,12 +186,6 @@ BEGIN
 	INSERT [dbo].[MembershipUsersInRoles] ([UserIdentifier], [RoleIdentifier]) VALUES ((SELECT id FROM MembershipUser WHERE UserName ='autoRemoveUser@test.co.uk'), (SELECT id FROM MembershipRole WHERE roleName='Standard Members'))
 
 	/**
-	* INSERT AUTOMATION SYSTEM PAGES
-	*/
-	INSERT [dbo].[SystemPage] ([Id], [Slug], [Title], [Content], [IsDeleted]) VALUES (N'3b2dee53-6f23-474c-b37a-adad00e4bdac', N'repeat-slug-test', N'Repeat Slug Test', N'<p>Random Content</p>
-	<p>&nbsp;</p>', 0)
-
-	/**
 	* INSERT AUTOMATION POSTS
 	*/
 	INSERT [dbo].[Comment] ([Entity_Id], [Content], [CreatedAtUTC], [ModifiedAtUTC], [InReplyTo], [Parent_EntityId], [CreatedBy], [ThreadId]) VALUES (N'ca5c5162-7a2a-482e-b15b-ad7f010e30cf', N'<p>Comment for Like test</p>', CAST(N'2021-08-09 16:23:44.043' AS DateTime2), CAST(N'2021-08-09 16:23:44.043' AS DateTime2), NULL, N'493a2f0e-4195-4730-9f3c-ad7f010d40c8', N'd74ed860-9ea5-4c95-9394-ad3a00924fa5', NULL)
@@ -242,8 +224,8 @@ BEGIN
 	INSERT [dbo].[Comment] ([Entity_Id], [Content], [CreatedAtUTC], [ModifiedAtUTC], [InReplyTo], [Parent_EntityId], [CreatedBy], [ThreadId]) VALUES (N'ce818d68-d4c5-4518-8b4f-982930b5f8c1', N'<p>Fifth Public Comment</p>', CAST(N'2022-03-03T13:59:45.0000000' AS DateTime2), NULL, NULL, N'eaf7a8f1-b9a0-415d-b26d-88517132536e', N'd74ed860-9ea5-4c95-9394-ad3a00924fa5', NULL)
 	INSERT [dbo].[Comment] ([Entity_Id], [Content], [CreatedAtUTC], [ModifiedAtUTC], [InReplyTo], [Parent_EntityId], [CreatedBy], [ThreadId]) VALUES (N'7120144f-a4a3-40dc-bd83-b7f8e7f8f66d', N'<p>Second Public Reply</p>', CAST(N'2022-03-03T14:00:30.0000000' AS DateTime2), NULL, N'3cb17bfa-9ad4-4ab7-9801-36aff0278be0', N'eaf7a8f1-b9a0-415d-b26d-88517132536e', N'd74ed860-9ea5-4c95-9394-ad3a00924fa5', N'5ca2c29e-31d6-448e-a409-6fbf83cbf844')
 	INSERT [dbo].[Comment] ([Entity_Id], [Content], [CreatedAtUTC], [ModifiedAtUTC], [InReplyTo], [Parent_EntityId], [CreatedBy], [ThreadId]) VALUES (N'468cf575-9dcd-4897-9976-f747009dc13d', N'<p>Fourth Public Comment</p>', CAST(N'2022-03-03T13:59:37.0000000' AS DateTime2), NULL, NULL, N'eaf7a8f1-b9a0-415d-b26d-88517132536e', N'd74ed860-9ea5-4c95-9394-ad3a00924fa5', NULL)
-	INSERT [dbo].[Comment] ([Entity_Id], [Content], [CreatedAtUTC], [ModifiedAtUTC], [InReplyTo], [Parent_EntityId], [CreatedBy], [ThreadId]) VALUES (N'4654a062-b693-42f5-9b25-c8a286fa8ba9', N'<p>Seventh Public Comment</p>', CAST(N'2022-03-03T13:59:57.0000000' AS DateTime2), NULL, NULL, N'eaf7a8f1-b9a0-415d-b26d-88517132536e', N'd74ed860-9ea5-4c95-9394-ad3a00924fa5', NULL)
-
+	INSERT [dbo].[Comment] ([Entity_Id], [Content], [CreatedAtUTC], [ModifiedAtUTC], [InReplyTo], [Parent_EntityId], [CreatedBy], [ThreadId]) VALUES (N'DAC3D7BF-8429-410F-93F7-1A25FC9E42F2', N'<p>Reply for FNHS56 test scenario</p>', CAST(N'2022-06-16 09:57:18.000' AS DateTime2), NULL, N'1AF57D07-9012-4511-B9FC-AD88012033B0', N'96B155BA-E8DE-40E3-980E-AD8800B0A6B8', N'd74ed860-9ea5-4c95-9394-ad3a00924fa5', N'1AF57D07-9012-4511-B9FC-AD88012033B0')
+		
 	/**
 	* INSERT AUTOMATION Discussions
 	*/
@@ -263,55 +245,10 @@ BEGIN
 	/**
 	* INSERT ENTITY IDS INTO ENTITY
 	*/
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'ca5c5162-7a2a-482e-b15b-ad7f010e30cf')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'edcc5437-b704-45d8-8972-ad7f010e2316')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'e56abf3c-7aa0-465a-a146-ad7f010e4acc')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'bcca8784-0e5e-496c-9bbb-ad7f010e51b4')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'e846a7bc-327a-4c05-89c7-ad7f010e5ed2')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'e4940ced-087d-41e9-a350-ad7f010e6bc9')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'69f7c324-3c63-4917-8b22-ad7f010e719e')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'58ab24e5-f3f9-4f8d-aba9-ad7f010e809d')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'107d1f85-3366-410c-8ffe-ad7f010e8586')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'0e807603-6d32-4f4f-833b-ad7f010e903a')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'e28ed14b-46ae-4fdd-8988-ad7f010e959c')
-	INSERT [dbo].[Entity] ([Id]) VALUES ((SELECT Entity_Id FROM Comment WHERE Content ='<p>First Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'))
-	INSERT [dbo].[Entity] ([Id]) VALUES ((SELECT Entity_Id FROM Comment WHERE Content ='<p>Third Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'))
-	INSERT [dbo].[Entity] ([Id]) VALUES ((SELECT Entity_Id FROM Comment WHERE Content ='<p>Fourth Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'))
-	INSERT [dbo].[Entity] ([Id]) VALUES ((SELECT Entity_Id FROM Comment WHERE Content ='<p>Fifth Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'))
-	INSERT [dbo].[Entity] ([Id]) VALUES ((SELECT Entity_Id FROM Comment WHERE Content ='<p>Sixth Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'))
-	INSERT [dbo].[Entity] ([Id]) VALUES ((SELECT Entity_Id FROM Comment WHERE Content ='<p>Seventh Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'))
-	INSERT [dbo].[Entity] ([Id]) VALUES ((SELECT Entity_Id FROM Comment WHERE Content ='<p>Eighth Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'))
-	INSERT [dbo].[Entity] ([Id]) VALUES ((SELECT Entity_Id FROM Comment WHERE Content ='<p>Ninth Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'))
-	INSERT [dbo].[Entity] ([Id]) VALUES ((SELECT Entity_Id FROM Comment WHERE Content ='<p>Tenth Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'))
-	INSERT [dbo].[Entity] ([Id]) VALUES ((SELECT Entity_Id FROM Comment WHERE Content ='<p>Eleventh Comment</p>' AND Parent_EntityId = '83702899-fdbf-49ed-bb1d-adda00f757c3'))
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'80d3fc63-4852-4163-98ac-ad880118903c')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'5313414a-ac9d-49eb-af90-ad880118b1ca')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'b578b2e5-cce4-4e84-9e64-ad880118bce2')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'c30117fc-dbb5-453c-9968-ad880118c70e')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'1af57d07-9012-4511-b9fc-ad88012033b0')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'4760e89f-048e-4b47-a70e-339cd43a2703')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'3cb17bfa-9ad4-4ab7-9801-36aff0278be0')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'5ca2c29e-31d6-448e-a409-6fbf83cbf844')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'9652f206-bd13-4920-b6d4-76858a21029a')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'13fa753a-fcba-4bf3-ae4c-771103503246')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'c0011070-c541-450e-a19f-7b4f91640835')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'285a2aa3-bfb8-41e1-b4d1-9349a68f2252')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'ce818d68-d4c5-4518-8b4f-982930b5f8c1')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'7120144f-a4a3-40dc-bd83-b7f8e7f8f66d')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'468cf575-9dcd-4897-9976-f747009dc13d')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'4654a062-b693-42f5-9b25-c8a286fa8ba9')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'493a2f0e-4195-4730-9f3c-ad7f010d40c8')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'3fbc0d4e-c2a7-46a0-99e9-ad7f010eb4ad')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'a4bf9b51-d145-4601-92b6-ad7f010ebe84')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'6ea66f95-24bc-4179-94f4-ad7f010ec730')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'a6da1550-066f-4ae6-ae98-ad7f010ece99')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'e1d477d3-dfb0-4acd-b74d-ad7f010ed722')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'55f516e0-9117-4a43-9363-ad8200ce7d1a')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'96b155ba-e8de-40e3-980e-ad8800b0a6b8')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'83702899-fdbf-49ed-bb1d-adda00f757c3')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'5889c069-5061-403b-93f6-adda00f7638a')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'f91c0e75-9d3d-4700-9efb-adda00f770d0')
-	INSERT [dbo].[Entity] ([Id]) VALUES (N'eaf7a8f1-b9a0-415d-b26d-88517132536e')
+	INSERT INTO [dbo].[Entity] SELECT Entity_Id FROM Comment WHERE CreatedBy IN (SELECT * FROM #seededUsers);
+	INSERT INTO [dbo].[Entity] SELECT Entity_Id FROM Discussion WHERE CreatedBy IN (SELECT * FROM #seededUsers);
+	    
+	DROP TABLE #seededUsers
 	
     -- Re-enable constraints for all tables:
     EXEC sp_msforeachtable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all';	
