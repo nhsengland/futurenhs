04da364377f63d55c863719ffb39ad71a69c4f40
Alex Grubba <alex.grubba@cds.co.uk>
2022-06-23 14:32:27 +0100

    [*] comments for all new editablePages functions

--- futurenhs.automation/pageObjects/editablePages.js
+++ futurenhs.automation/pageObjects/editablePages.js
@@ -3,7 +3,12 @@ const helpers = require('../util/helpers');
 const basePage = require('./basePage');
 
 class editablePages extends basePage {
-    
+    /**
+     * Switch function to find the desired block, then move to the correct type of expected validation. Control for future expansion
+     * @param {*} blockType - type of desired block
+     * @param {*} blockState - the state in which to validate
+     * @param {*} instance - instance IF specified of the block
+     */
     blockValidationSelect(blockType, blockState, instance) {
         console.log('instance', instance)
         var foundBlock = this.blockSelect(blockType, instance);
@@ -16,6 +21,12 @@ class editablePages extends basePage {
         }
     }
     
+    /**
+     * Function to locate the desired block in the html and validate it exists 
+     * @param {*} blockType - type of desired block
+     * @param {*} instance - instance IF specified of the block
+     * @returns - found element within the html
+     */
     blockSelect(blockType, instance) {
         blockType = _.camelCase(blockType);
         var blockPath = `//div[@data-content-type-id="${blockType}"]`
@@ -28,14 +39,28 @@ class editablePages extends basePage {
         return desiredBlock;
     }
 
+    /**
+     * Validates a block that is currently in Preview Mode by confirming no form exists within
+     * @param {*} foundBlock - Block element found during blockSelect
+     */
     previewModeValidation(foundBlock) {
         foundBlock.$(`./div//form/div[@class="c-form_body"]`).waitForDisplayed({reverse: true});
     }
 
+    /**
+     * Validates a block that is currently in Edit Mode by confirming a form exists within
+     * @param {*} foundBlock - Block element found during blockSelect
+     */
     editModeValidation(foundBlock) {
         helpers.waitForLoaded(foundBlock.$(`./div//form/div[@class="c-form_body"]`));
     }
 
+    /**
+     * Finds all enabled action buttons within the desired block, validates those found match what was expected
+     * @param {*} blockType - type of desired block
+     * @param {*} expectedButtons - Array of text values for all expected enabled buttons at time of testing
+     * @param {*} instance - instance IF specified of the block
+     */
     enabledActionsValidation(blockType, expectedButtons, instance) {
         var desiredBlock = this.blockSelect(blockType, instance);
         var enabledButtons, foundButtons = []
@@ -49,6 +74,12 @@ class editablePages extends basePage {
         });
     }
 
+    /**
+     * Locates and Clicks a desired button within the specified block
+     * @param {*} blockType - type of desired block
+     * @param {*} buttonToClick - textual value of the desired button to be clicked
+     * @param {*} instance - instance IF specified of the block
+     */
     blockActionClick(blockType, buttonToClick, instance) {
         var desiredBlock = this.blockSelect(blockType, instance);
         var blockButton = desiredBlock.$(`./header//button[starts-with(normalize-space(.), "${buttonToClick}")]`);
