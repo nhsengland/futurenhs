f90a9e2ff6a6a8b01406c40a3de8f95f13389165
ReallySmall <richard.iles@digital.cds.uk>
2022-06-24 14:46:53 +0100

    [*] Updates signin and signout route content

--- futurenhs.app/components/_pageTemplates/AuthSignOutTemplate/index.tsx
+++ futurenhs.app/components/_pageTemplates/AuthSignOutTemplate/index.tsx
@@ -1,34 +1,42 @@
-import { PageBody } from '@components/PageBody';
-import { RichText } from '@components/RichText';
-import Link from 'next/link';
+import { PageBody } from '@components/PageBody'
+import { RichText } from '@components/RichText'
+import { LayoutColumnContainer } from '@components/LayoutColumnContainer'
+import { LayoutColumn } from '@components/LayoutColumn'
 
 import { Props } from './interfaces'
+import Link from 'next/link'
 
 /**
  * Auth signout template
  */
 export const AuthSignOutTemplate: (props: Props) => JSX.Element = ({
     routes,
-    contentText
+    contentText,
 }) => {
-
-    const { authSignIn } = routes ?? {};
-    const { mainHeading, bodyHtml, signIn } = contentText ?? {};
+    const { authSignIn } = routes ?? {}
+    const { mainHeading, intro, signIn } = contentText ?? {}
 
     return (
-
         <PageBody className="tablet:u-px-0">
-            <h1 className="nhsuk-heading-xl">{mainHeading}</h1>
-            {bodyHtml &&
-                <RichText wrapperElementType="div" className="u-mb-10" bodyHtml={bodyHtml} />
-            }
-            {signIn &&
-                <Link href={authSignIn}>
-                    <a className="c-button">{signIn}</a>
-                </Link>
-            }
+            <LayoutColumnContainer justify="centre">
+                <LayoutColumn tablet={8} desktop={6}>
+                    {mainHeading && (
+                        <h1 className="nhsuk-heading-xl">{mainHeading}</h1>
+                    )}
+                    {intro && (
+                        <RichText
+                            wrapperElementType="div"
+                            className="u-mb-8"
+                            bodyHtml={intro}
+                        />
+                    )}
+                    {authSignIn && signIn && (
+                        <Link href={authSignIn}>
+                            <a className="c-button">{signIn}</a>
+                        </Link>
+                    )}
+                </LayoutColumn>
+            </LayoutColumnContainer>
         </PageBody>
-
     )
-
 }
--- futurenhs.app/pages/auth/signout/index.page.tsx
+++ futurenhs.app/pages/auth/signout/index.page.tsx
@@ -1,19 +1,19 @@
-import { GetServerSideProps } from 'next';
-import { getSession } from 'next-auth/react';
+import { GetServerSideProps } from 'next'
+import { getSession } from 'next-auth/react'
 
-import { setFetchOpts, fetchJSON } from '@helpers/fetch';
-import { getAuthCsrfData } from '@services/getAuthCsrfData';
+import { setFetchOpts, fetchJSON } from '@helpers/fetch'
+import { getAuthCsrfData } from '@services/getAuthCsrfData'
 import { handleSSRSuccessProps } from '@helpers/util/ssr/handleSSRSuccessProps'
 import { handleSSRErrorProps } from '@helpers/util/ssr/handleSSRErrorProps'
 import { withRoutes } from '@hofs/withRoutes'
 import { withTextContent } from '@hofs/withTextContent'
 import { GetServerSidePropsContext } from '@appTypes/next'
 
-import { AuthSignOutTemplate } from '@components/_pageTemplates/AuthSignOutTemplate';
-import { Props } from '@components/_pageTemplates/AuthSignOutTemplate/interfaces';
-import { defaultTimeOutMillis, requestMethods } from '@constants/fetch';
+import { AuthSignOutTemplate } from '@components/_pageTemplates/AuthSignOutTemplate'
+import { Props } from '@components/_pageTemplates/AuthSignOutTemplate/interfaces'
+import { defaultTimeOutMillis, requestMethods } from '@constants/fetch'
 
-const routeId: string = '043b4409-7aa4-4d9d-af9b-30cb0b469f02';
+const routeId: string = '043b4409-7aa4-4d9d-af9b-30cb0b469f02'
 const props: Partial<Props> = {}
 
 /**
@@ -25,33 +25,34 @@ export const getServerSideProps: GetServerSideProps = withRoutes({
         props,
         routeId,
         getServerSideProps: async (context: GetServerSidePropsContext) => {
-
-            const { query } = context;
-            const session = await getSession(context);
+            const { query } = context
+            const session = await getSession(context)
 
             /**
-             * Hide breadcrumbs
+             * Set base template properties
              */
-            (props as any).breadCrumbList = [];
+            ;(props as any).breadCrumbList = []
+            ;(props as any).shouldRenderMainNav = false
+            ;(props as any).className = 'u-bg-theme-3'
 
             /**
              * Redirect to site root if already signed out
              */
             if (session) {
-
                 /**
                  * Get data from services
                  */
                 try {
-
-                    const callbackUrl: string = `${process.env.APP_URL}${context.resolvedUrl}`;
-                    const idTokenHint: string = (session.id_token as string);
+                    const callbackUrl: string = `${process.env.APP_URL}${context.resolvedUrl}`
+                    const idTokenHint: string = session.id_token as string
 
                     /**
                      * Get next-auth specific csrf token and associated cookie header
                      */
-                    const [csrfData] = await Promise.all([getAuthCsrfData({ query })]);
-                    const csrfToken: string = csrfData.data;
+                    const [csrfData] = await Promise.all([
+                        getAuthCsrfData({ query }),
+                    ])
+                    const csrfToken: string = csrfData.data
 
                     /**
                      * Sign out locally
@@ -60,7 +61,7 @@ export const getServerSideProps: GetServerSideProps = withRoutes({
                         `${process.env.APP_URL}${props.routes.authApiSignOut}`,
                         setFetchOpts({
                             method: requestMethods.POST,
-                            body: { csrfToken, callbackUrl }
+                            body: { csrfToken, callbackUrl },
                         }),
                         defaultTimeOutMillis
                     )
@@ -68,7 +69,10 @@ export const getServerSideProps: GetServerSideProps = withRoutes({
                     /**
                      * Ensure the request to clear the token is passed through to the browser
                      */
-                    context.res.setHeader('Set-Cookie', 'next-auth.session-token=; path=/; max-age=0');
+                    context.res.setHeader(
+                        'Set-Cookie',
+                        'next-auth.session-token=; path=/; max-age=0'
+                    )
 
                     /**
                      * Sign out on Azure
@@ -77,13 +81,11 @@ export const getServerSideProps: GetServerSideProps = withRoutes({
                         redirect: {
                             permanent: false,
                             destination: `https://${process.env.AZURE_AD_B2C_TENANT_NAME}.b2clogin.com/${process.env.AZURE_AD_B2C_TENANT_NAME}.onmicrosoft.com/${process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW}/oauth2/v2.0/logout?post_logout_redirect_uri=${callbackUrl}&id_token_hint=${idTokenHint}`,
-                        }
+                        },
                     }
-
                 } catch (error) {
                     return handleSSRErrorProps({ props, error })
                 }
-
             }
 
             /**
