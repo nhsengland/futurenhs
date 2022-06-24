8fa1a3fea69bf560c678fe88a3d63eb930617ee2
ReallySmall <richard.iles@digital.cds.uk>
2022-06-24 14:46:53 +0100

    [*] Updates signin and signout route content

--- futurenhs.app/content-configs/043b4409-7aa4-4d9d-af9b-30cb0b469f02.ts
+++ futurenhs.app/content-configs/043b4409-7aa4-4d9d-af9b-30cb0b469f02.ts
@@ -3,7 +3,7 @@ import { GenericPageTextContent } from '@appTypes/content'
 export default {
     title: 'Signed out',
     metaDescription: 'Signed out',
-    mainHeading: 'You\'re signed out',
-    bodyHtml: '<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>',
-    signIn: 'Sign in again'
+    mainHeading: 'Signed out',
+    intro: '<p>Collaborate, connect, share and learn.</p>',
+    signIn: 'Sign in again',
 } as GenericPageTextContent
