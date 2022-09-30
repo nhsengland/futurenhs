7b293d95892dd0e48300736573dd28bd88da8ddc
Rio Knightley <rio.knightley@madetech.com>
2022-09-30 14:13:32 +0100

    Add next auth sign out hook

--- futurenhs.app/pages/auth/signout/index.page.tsx
+++ futurenhs.app/pages/auth/signout/index.page.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps } from 'next'
-import { getSession } from 'next-auth/react'
+import { getSession, signOut } from 'next-auth/react'
 import { pipeSSRProps } from '@helpers/util/ssr/pipeSSRProps'
 import { setFetchOpts, fetchJSON } from '@helpers/fetch'
 import { getAuthCsrfData } from '@services/getAuthCsrfData'
@@ -15,84 +15,76 @@ import { defaultTimeOutMillis, requestMethods } from '@constants/fetch'
 
 export const getServerSideProps: GetServerSideProps = async (
     context: GetServerSidePropsContext
-) =>
-    await pipeSSRProps(
-        context,
-        {
-            routeId: '043b4409-7aa4-4d9d-af9b-30cb0b469f02',
-        },
-        [withRoutes, withTextContent],
-        async (context: GetServerSidePropsContext) => {
-            const props: Partial<Props> = selectPageProps(context)
-            const { query } = context
-            const session = await getSession(context)
+) => {
+    const props: Partial<Props> = selectPageProps(context)
+    const { query } = context
+    const session = await getSession(context)
+
+    /**
+     * Set base template properties
+     */
+    ;(props as any).breadCrumbList = []
+    ;(props as any).shouldRenderMainNav = false
+    ;(props as any).className = 'u-bg-theme-3'
+
+    /**
+     * Redirect to site root if already signed out
+     */
+    if (session) {
+        /**
+         * Get data from services
+         */
+        try {
+            const callbackUrl: string = `${process.env.APP_URL}${context.resolvedUrl}`
+            const idTokenHint: string = session.id_token as string
 
             /**
-             * Set base template properties
+             * Get next-auth specific csrf token and associated cookie header
              */
-            ;(props as any).breadCrumbList = []
-            ;(props as any).shouldRenderMainNav = false
-            ;(props as any).className = 'u-bg-theme-3'
+            const [csrfData] = await Promise.all([getAuthCsrfData({ query })])
+            const csrfToken: string = csrfData.data
+
+            // /**
+            //  * Sign out locally
+            //  */
+            // await fetchJSON(
+            //     `${process.env.APP_URL}${props.routes.authApiSignOut}`,
+            //     setFetchOpts({
+            //         method: requestMethods.POST,
+            //         body: { csrfToken, callbackUrl },
+            //     }),
+            //     defaultTimeOutMillis
+            // )
+
+            await signOut()
 
             /**
-             * Redirect to site root if already signed out
+             * Ensure the request to clear the token is passed through to the browser
              */
-            if (session) {
-                /**
-                 * Get data from services
-                 */
-                try {
-                    const callbackUrl: string = `${process.env.APP_URL}${context.resolvedUrl}`
-                    const idTokenHint: string = session.id_token as string
+            context.res.setHeader(
+                'Set-Cookie',
+                'next-auth.session-token=; path=/; max-age=0'
+            )
 
-                    /**
-                     * Get next-auth specific csrf token and associated cookie header
-                     */
-                    const [csrfData] = await Promise.all([
-                        getAuthCsrfData({ query }),
-                    ])
-                    const csrfToken: string = csrfData.data
-
-                    // /**
-                    //  * Sign out locally
-                    //  */
-                    // await fetchJSON(
-                    //     `${process.env.APP_URL}${props.routes.authApiSignOut}`,
-                    //     setFetchOpts({
-                    //         method: requestMethods.POST,
-                    //         body: { csrfToken, callbackUrl },
-                    //     }),
-                    //     defaultTimeOutMillis
-                    // )
-
-                    /**
-                     * Ensure the request to clear the token is passed through to the browser
-                     */
-                    context.res.setHeader(
-                        'Set-Cookie',
-                        'next-auth.session-token=; path=/; max-age=0'
-                    )
-
-                    /**
-                     * Sign out on Azure
-                     */
-                    return {
-                        redirect: {
-                            permanent: false,
-                            destination: `https://${process.env.AZURE_AD_B2C_TENANT_NAME}.b2clogin.com/${process.env.AZURE_AD_B2C_TENANT_NAME}.onmicrosoft.com/${process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW}/oauth2/v2.0/logout?post_logout_redirect_uri=${callbackUrl}&id_token_hint=${idTokenHint}`,
-                        },
-                    }
-                } catch (error) {
-                    return handleSSRErrorProps({ props, error })
-                }
+            /**
+             * Sign out on Azure
+             */
+            return {
+                redirect: {
+                    permanent: false,
+                    destination: `https://${process.env.AZURE_AD_B2C_TENANT_NAME}.b2clogin.com/${process.env.AZURE_AD_B2C_TENANT_NAME}.onmicrosoft.com/${process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW}/oauth2/v2.0/logout?post_logout_redirect_uri=${callbackUrl}&id_token_hint=${idTokenHint}`,
+                },
             }
-
-            /**
-             * Return data to page template
-             */
-            return handleSSRSuccessProps({ props, context })
+        } catch (error) {
+            return handleSSRErrorProps({ props, error })
         }
-    )
+    }
+
+    /**
+     * Return data to page template
+     */
+    return handleSSRSuccessProps({ props, context })
+}
 
 /**
  * Export page template
