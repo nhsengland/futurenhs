cca160458ef61c082ce3621e24e7447420453c7b
Tim-Hunt <timothy.hunt@madetech.com>
2022-10-06 12:13:29 +0100

    Mock api endpoint for front end dev

--- futurenhs.api/FutureNHS.Api/Controllers/RegistrationController.cs
+++ futurenhs.api/FutureNHS.Api/Controllers/RegistrationController.cs
@@ -1,3 +1,4 @@
+using FutureNHS.Api.DataAccess.Models.Registration;
 using FutureNHS.Api.Models.UserInvite;
 using FutureNHS.Api.Services.Admin.Interfaces;
 using FutureNHS.Api.Services.Interfaces;
@@ -37,6 +38,29 @@ namespace FutureNHS.Api.Controllers
                 userInvite.EmailAddress, cancellationToken);
 
             return Ok();
+        }
+
+
+        [HttpGet]
+        [Route("registration/invite/{id:guid}")]
+        public async Task<IActionResult> GetRegistrationInviteDetailsAsync(Guid id, CancellationToken cancellationToken)
+        {
+            if (id != Guid.NewGuid())
+                throw new ArgumentNullException(nameof(id));
+
+            var mockData = new InviteDetails
+            {
+                Id = id,
+                Group = new InviteGroupSummary { 
+                    DiscussionCount = 324, 
+                    NameText = "Joe's super group", 
+                    StraplineText = "Some description about the group", 
+                    MemberCount = 3000, 
+                    Slug = "joes-group", 
+                    IsPublic = true },
+                InvitedBy = new DataAccess.Models.User.UserNavProperty { Id = Guid.NewGuid(), Name = "Joe Bloggs" }
+            };
+            return Ok(mockData);
         }
     }
 }
