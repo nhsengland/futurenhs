5af6aec37eada93e5d976f0a7f813a4f7429ab6c
Rio Knightley <rio.knightley@madetech.com>
2022-10-07 16:05:32 +0100

    Update error handling for add to group api

--- futurenhs.api/FutureNHS.Api/Services/Interfaces/IRegistrationService.cs
+++ futurenhs.api/FutureNHS.Api/Services/Interfaces/IRegistrationService.cs
@@ -5,9 +5,9 @@ namespace FutureNHS.Api.Services.Interfaces
 {
     public interface IRegistrationService
     {
-        Task InviteMemberToGroupAndPlatformAsync(Guid userId, string? groupSlug, string email, CancellationToken cancellationToken);
+        Task InviteMemberToGroupAndPlatformAsync(Guid userId, string groupSlug, string email, CancellationToken cancellationToken);
         
-        Task InviteMemberToPlatformAsync(Guid userId, string? groupSlug, string email, CancellationToken cancellationToken);
+        Task InviteMemberToPlatformAsync(Guid userId, string email, CancellationToken cancellationToken);
 
 
         Task<InviteDetails> GetRegistrationInviteDetailsAsync(Guid id, CancellationToken cancellationToken);
