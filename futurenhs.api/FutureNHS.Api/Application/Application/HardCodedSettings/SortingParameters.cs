a98085592907965a30a0d629ec5f5fa3ea0df50e
dave-t-MT <david.trainor@madetech.com>
2022-12-14 14:32:21 +0000

    add sorting to get discussions endpoint

---
+++ futurenhs.api/FutureNHS.Api/Application/Application/HardCodedSettings/SortingParameters.cs
@@ -0,0 +1,8 @@
+namespace FutureNHS.Api.Application.Application.HardCodedSettings;
+
+public static class SortingParameters
+{
+    public const string SortByName = "Name";
+    public const string SortByDateCreated = "DateCreated";
+    public const string SortByLastComment = "LastComment";
+}
